21042e6842aa0d97eb72a2887410814559d14f56
asddataking <asddataking@outlook.com>
2022-03-15 18:05:47 -0400

    completed version 2

--- code/dan/python/lab01.py
+++ code/dan/python/lab01.py
@@ -1,5 +1,16 @@
-def converter ():
- feet = input ("What is the distance in feet?")   
- print (feet * 0.3048)   
+distance = input ("What is the distance?")  
+from_units = input ("What are the units?")
+#to_units = input ("What are the output units?")
 
+def converter (distance, from_units):
+  if from_units == "ft": 
+    x = (int(distance) * 0.3048)
+    print (float(x)) 
+  elif from_units == "km":
+    x = (int(distance) * 1000)
+    print (float(x))
+  elif from_units == "mi":
+    x = (int(distance) * 1609.34)
+    print (float(x))
 
+converter(distance, from_units)
