26a0fcc2dcdb9aee16e5ae9adb98f3ca0b71b805
logandouglass <logandouglass@gmail.com>
2022-03-16 15:56:18 -0400

    fixed for numbers > 100 that have a zero i nthe tens column.  Also, sorry!  New to git and have been doing pull requests to quickly.  I'll add some comments for readability to the labs I've already submitted.

--- code/logan/lab04.py
+++ code/logan/lab04.py
@@ -57,6 +57,7 @@
 #     print(stringwriter(num_input))
 
 ##Version 2##
+# Here are a bunch of variables
 special = False
 d_ones = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
 d_tens = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"}
@@ -66,11 +67,10 @@ num_input = int(input("\nPlease input a number 0 - 999:  "))
 snum_input = str(num_input)
 ones = num_input % 10
 tens = num_input // 10
-# stens = (num_input / 10) // 10
 hundreds = num_input // 100
 print("\n")
 
-
+# This returns #s that aren't special(0, 11-19) and less than 100 as a string.
 def stringwriter(x):
     if x < 10:
         return(d_ones[ones])
@@ -80,18 +80,21 @@ def stringwriter(x):
         else:
             return(d_tens[tens] + "-" + d_ones[ones])
 
+# This returns numbers that aren't special (0, 11-19) and >= 100 as a string.
 def superstringwriter():
     special_check = int(snum_input[1] + snum_input[2])
     if (snum_input[1] == "0" and snum_input[2] == "0"):
         return(d_hundreds[hundreds])
     elif (snum_input[1] != "0" and snum_input[2] == "0"):
         return(d_hundreds[hundreds] + " and " + d_tens[int(snum_input[1])])
+    elif (snum_input[1] == "0" and snum_input[2] != "0"):
+        return(d_hundreds[hundreds]) + " and " + d_ones[int(snum_input[2])]
     elif special_check in d_special:
         return(d_hundreds[hundreds] + " and " + d_special[special_check])
     else:
         return(d_hundreds[hundreds] + " and " + d_tens[int(snum_input[1])] + "-" + d_ones[ones])
 
-##The Specials 0, 10 - 19##
+##The Specials 0, 10 - 19 -- these have no good, regular convention for generation##
 if num_input == 0:
     print("zero")
     special = True
