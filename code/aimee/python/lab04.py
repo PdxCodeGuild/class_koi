668ef26a3f9e9cc6a44a4b71efd314af33cb0dd3
Aimee <aimeenyy@gmail.com>
2022-03-17 14:56:56 -0700

    completed lab4 versions 1 & 2

--- code/aimee/python/lab04.py
+++ code/aimee/python/lab04.py
@@ -35,13 +35,7 @@
 #     7:'seventy',
 #     8:'eighty',
 #     9:'ninety',
-#     40:'forty',
-#     50:'fifty',
-#     60:'sixty',
-#     70:'seventy',
-#     80:'eighty',
-#     90:'ninety',
-# }
+#     }
 
 # while True:
 #     x = int(input('Enter a number between 0 and 99: '))
@@ -99,31 +93,40 @@ tens = {
     7:'seventy',
     8:'eighty',
     9:'ninety',
-    40:'forty',
-    50:'fifty',
-    60:'sixty',
-    70:'seventy',
-    80:'eighty',
-    90:'ninety',
+}
+
+hundreds = {
+    1:'one hundred',
+    2:'two hundred',
+    3:'three hundred',
+    4:'four hundred',
+    5:'five hundred',
+    6:'six hundred',
+    7:'seven hundred',
+    8:'eight hundred',
+    9:'nine hundred',
 }
 
 while True:
-    x = int(input('Enter a number between 0 and 99: '))
-    if x < 0 or x > 99:
-        print('Please enter a number between 0 and 99.')
+    x = int(input('Enter a number between 0 and 999: '))
+    if x < 0 or x > 999:
+        print('Please enter a number between 0 and 999.')
     else:
         break
 
-if x >= 10 and x <= 20:
-    print(teens[x]) # printing between 10 and 20
+if x % 100 >= 10 and x % 100 <= 20:
+    hundreds_digit = x // 100
+    print(hundreds[hundreds_digit] + teens[x]) # printing between 10 and 20, 110 and 120
 elif x >= 0 and x <= 9:
     print(ones[x]) # printing between 0 and 9
-else: 
-    tens_digit = x // 10
-    ones_digit = x % 10 
-    hundreds_digit = x // 100 # if x were 300, dividing x by 100 would give the one digit - 3 + 'hundred'
-    print(tens[tens_digit] + ' ', ones[ones_digit]) # between 21 and 99
-
+else: # if x = 455
+    hundreds_digit = x // 100 # 455 // 100 = 400
+    x %= 100 # 455 % 100 = 55
+    tens_digit = x // 10 # 455 // 10 = 45
+    ones_digit = x % 10 # 455 % 10 = 5
+     # if x were 300, dividing x by 100 would give the one digit - 3 + 'hundred'
+    print(f'{ones[hundreds_digit]} hundred {tens[tens_digit]} {ones[ones_digit]}') # between 21 and 999
+   
 
 
 
