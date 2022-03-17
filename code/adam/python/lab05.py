00b71dfcdeff3a34a9598e5dff43574ac5399b3b
AdamFraley <adamfraley@gmail.com>
2022-03-17 13:42:40 -0700

    completed lab05 about about to PR

--- code/adam/python/lab05.py
+++ code/adam/python/lab05.py
@@ -2,9 +2,9 @@
 # Version 1
 
 print('Possible card inputs: A, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K')
-card_1 = input("What's your first card? ")
-card_2 = input("What's your second card? ")
-card_3 = input("What's your third card? ")
+card_1 = input("What's your first card? ").upper()
+card_2 = input("What's your second card? ").upper()
+card_3 = input("What's your third card? ").upper()
 
 card_value = {
     'A': 1,
@@ -25,4 +25,11 @@ card_value = {
 
 hand_value = card_value[card_1] + card_value[card_2] + card_value[card_3]
 
-print(hand_value)
+if hand_value == 21:
+    print(f'{hand_value} Blackjack!')
+elif hand_value < 17:
+    print(f'{hand_value} Hit')
+elif hand_value < 21:
+    print(f'{hand_value} Stay')
+else:
+    print(f'{hand_value} "Already Busted"')
