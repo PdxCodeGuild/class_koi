5f43ed9bf4d03d7488d1cae109423b11221476d2
nbonilla <nbonilla409@gmail.com>
2022-03-17 14:09:35 -0700

    updated lab05 version 1 and added version 2

--- code/nathan/python/lab05.py
+++ code/nathan/python/lab05.py
@@ -2,22 +2,22 @@
 Let's write a python program to give basic blackjack playing advice during a game by asking the player for cards.
 '''
 
-def cardConversion(card):
-    if(card == 'A'):
-        return 1
-    
-    elif(card == 'J' or card == 'Q' or card == 'K'):
-        return 10
-    
-    return int(card)
-
 def versionOne():
+
+    def cardConversion(card):
+        if(card == 'A'):
+            return 1
+    
+        elif(card == 'J' or card == 'Q' or card == 'K'):
+            return 10
+    
+        return int(card)
 
     result = 0
 
     firstCard = input("What's your first card? ").upper()
     secondCard = input("What's your second card? ").upper()
-    thirdCard = input("What's your second card? ").upper()
+    thirdCard = input("What's your third card? ").upper()
 
     firstCard = cardConversion(firstCard)
     secondCard = cardConversion(secondCard)
@@ -30,8 +30,55 @@ def versionOne():
     elif(result >= 17 and result < 21):
         print(f"{result} Stay")
     elif(result == 21):
-        print(f"Blackjack!")
+        print(f"{result} Blackjack!")
     elif(result > 21):
-        print(f"Already Busted")
+        print(f"{result} Already Busted")
 
-versionOne()
+def versionTwo():
+
+    def cardConversion(card):
+        if(card == 'A'):
+            return 11
+    
+        elif(card == 'J' or card == 'Q' or card == 'K'):
+            return 10
+    
+        return int(card)
+
+    cardList = []
+    
+    result = 0
+
+    firstCard = input("What's your first card? ").upper()
+    secondCard = input("What's your second card? ").upper()
+    thirdCard = input("What's your third card? ").upper()
+
+    cardList.append(cardConversion(firstCard))
+    cardList.append(cardConversion(secondCard))
+    cardList.append(cardConversion(thirdCard))
+
+    result = sum(cardList) # Store user's cards into a list
+
+    # If the user's hand has aces and has a bust, convert aces to value of 1
+    if(11 in cardList and result > 21):
+        count = 0
+
+        # The user may have multiple aces which could cause a bust
+        while(result > 21 or count < len(cardList)):
+            if(cardList[count] == 11):
+                cardList[count] = 1
+                result = sum(cardList)
+
+            count += 1
+
+    if(result < 17):
+        print(f"{result} Hit")
+    elif(result >= 17 and result < 21):
+        print(f"{result} Stay")
+    elif(result == 21):
+        print(f"{result} Blackjack!")
+    elif(result > 21):
+        print(f"{result} Already Busted")
+
+# versionOne()
+versionTwo()
