f36eee033f7902efd03c2988052bf2f5813a1a18
MattDWalsh <matthew.david.walsh@gmail.com>
2022-03-18 07:49:26 -0700

    finished lcr_simulator.py

--- code/matt/python/optional/lcr_simulator.py
+++ code/matt/python/optional/lcr_simulator.py
@@ -32,51 +32,50 @@ while True: # ask how many players are playing, validate entry, and add each pla
     except:
         print('Please enter your answer as a number.')
 
-
-
-'''
-throw dice - forgot to add multiple throws per player/based on number of chips
----dice options are 1:l 3:c 5:r
-
-do dice evaluation and chip adjustment
-
-check for number of players with non-zero chip totals
-    if > 0 keep playing
-    if == 1 stop, declare winner
-
-'''
-
-
-players_with_chips = num_players
-current_player = 0
+players_with_chips = num_players # set the initial number of players with chips to the total number of players
+current_player = 0 # set the starting player
+throw = [] # empty list to hold dice throws
 
 while True:
-    while players_with_chips > 1:
-        right_player = current_player - 1
-        if current_player + 1 > num_players - 1:
+    while players_with_chips > 1: # continue looping as long as more than 1 player has chips
+        right_player = current_player - 1 # create variable as alias for player to the right
+        if current_player + 1 > num_players - 1: # create variable as alias for player to the left, avoiding index errors
             left_player = current_player - num_players + 1
         else:
             left_player = current_player + 1
-        throw = rand(1,6) # throw dice
-        print(throw) ##### debug, delete later #####
-        if throw == 1:
-            player_list[current_player][1] -= 1
-            player_list[left_player][1] += 1
-            print(player_list) ##### debug, delete later #####
-        elif throw == 3:
-            player_list[current_player][1] -= 1
-            print(player_list) ##### debug, delete later #####
-        elif throw == 5:
-            player_list[current_player][1] -= 1
-            player_list[right_player][1] += 1
-            print(player_list) ##### debug, delete later #####
-        current_player = left_player
+        
+        # throw dice up to 3 times based on number of chips in hand
+        for x in range(player_list[current_player][1] if player_list[current_player][1] <3 else 3):
+            throw.append(rand(1,6))
+
+        # adjust chip totals based on throw - "1" = "L", "3" = "C", "5" = "R"
+        for x in throw:
+            if x == 1:
+                player_list[current_player][1] -= 1
+                player_list[left_player][1] += 1
+            elif x == 3:
+                player_list[current_player][1] -= 1
+            elif x == 5:
+                player_list[current_player][1] -= 1
+                player_list[right_player][1] += 1
+
+        throw.clear() # empty throw list for use next loop
+
+        current_player = left_player # set current player to player to the left for next loop
+
+        # clear player_with_chips variable, then recalculate from player_list
         players_with_chips = 0
         for player in player_list:
-            print(player) ##### debug, delete later #####
             if player[1] > 0:
                 players_with_chips += 1
-    break
     
-print(player_list) ##### debug, delete later #####
-print(players_with_chips) ##### debug, delete later #####
+    # once higher loop clears when only 1 player has chips, find and declare winner.
+    for player in player_list:
+        if player[1] > 0:
+            print(f'\nThe winner is {player[0]} with {player[1]} {"chips" if player[1] > 1 else "chip"}!\n')
+
+    # allow player to continue or quit
+    quit = input('Would you like to play again? Enter "Y" to continue: ')
+    if quit.upper() != "Y":
+        print('Goodbye!')
+        break
