c0ae5873554bbeec72db2efa228d0174d232ddbf
Aimee <aimeenyy@gmail.com>
2022-03-18 14:25:05 -0700

    actually correct lab4 ver 1 & 2

--- code/aimee/python/lab04.py
+++ code/aimee/python/lab04.py
@@ -95,18 +95,6 @@ tens = {
     9:'ninety',
 }
 
-hundreds = {
-    1:'one hundred',
-    2:'two hundred',
-    3:'three hundred',
-    4:'four hundred',
-    5:'five hundred',
-    6:'six hundred',
-    7:'seven hundred',
-    8:'eight hundred',
-    9:'nine hundred',
-}
-
 while True:
     x = int(input('Enter a number between 0 and 999: '))
     if x < 0 or x > 999:
@@ -114,29 +102,54 @@ while True:
     else:
         break
 
-if x % 100 >= 10 and x % 100 <= 20:
-    hundreds_digit = x // 100
-    print(ones[hundreds_digit] + teens[x]) # printing between 10 and 20, 110 and 120
-elif x >= 0 and x <= 9:
-    print(ones[x]) # printing between 0 and 9
-else: # if x = 455
-    hundreds_digit = x // 100 # 455 // 100 = 400
-    x %= 100 # 455 % 100 = 55
-    tens_digit = x // 10 # 455 // 10 = 45
-    ones_digit = x % 10 # 455 % 10 = 5
-     # if x were 300, dividing x by 100 would give the one digit - 3 + 'hundred'
-    print(f'{ones[hundreds_digit]} hundred {tens[tens_digit]} {ones[ones_digit]}') # between 21 and 999
-   
+hundreds_digit = x // 100
+tens_digit = x // 10 % 10 # floor divide 450 by 10, 
+ones_digit = x % 10 
+teens_digit = x % 100 # incorrect
+
+if x >= 0 and x <= 9:
+    print(ones[x]) # works
+elif x >= 10 and x < 20:
+    print(teens[x]) # works
+elif x >= 21 and x <= 99:
+    print(tens[tens_digit] + ' ', ones[ones_digit]) # works
+elif x == 100 or x == 200 or x == 300 or x == 400 or x == 500 or x == 600 or x == 700 or x == 800 or x == 900:
+    print(f'{ones[hundreds_digit]} hundred') # works
+elif tens_digit == 1 and x > 100:
+    print(f'{ones[hundreds_digit]} hundred {teens[teens_digit]}')
+else:
+    if x >= 101 and x <= 999: # i.e. 11
+        print(f'{ones[hundreds_digit]} hundred {tens[tens_digit]} {ones[ones_digit]}') # works
+# getting tens key error
 
 
+# hundreds = {
+#     1:'one hundred',
+#     2:'two hundred',
+#     3:'three hundred',
+#     4:'four hundred',
+#     5:'five hundred',
+#     6:'six hundred',
+#     7:'seven hundred',
+#     8:'eight hundred',
+#     9:'nine hundred',
+# }
 
-
-
-
-
-
-
-
-
-
+# if x >= 10 and x < 20:
+#     print(teens[x])
+# elif x > 20 and x <= 99:
+#     print(tens[tens_digit] + ' ' + ones[ones_digit])
+# #elif x >= 110 and x <= 120:
+#     #print(f'{ones[hundreds_digit]} {tens[tens_digit]} {ones[ones_digit]}') # printing 110 and 120
+# elif x >= 0 and x <= 9:
+#     print(ones[x]) # printing between 0 and 9
+# elif x > 100 and tens_digit == 1:
+#     print(f'{ones[hundreds_digit]} hundred {teens[tens_digit]}')
+# else: # if x = 455
+#     hundreds_digit = x // 100 # 455 // 100 = 400
+#     x %= 100 # 455 % 100 = 55
+#     tens_digit = x // 10 # 455 // 10 = 45
+#     ones_digit = x % 10 # 455 % 10 = 5
+#      # if x were 300, dividing x by 100 would give the one digit - 3 + 'hundred'
+#     print(f'{ones[hundreds_digit]} hundred {tens[tens_digit]} {ones[ones_digit]}') # between 21 and 999
 
