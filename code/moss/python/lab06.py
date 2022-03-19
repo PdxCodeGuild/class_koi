93d31de653acca7e6a7734dcd9b54fa6e02f2d50
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-18 22:19:30 -0700

    Lab06 Pick6

---
+++ code/moss/python/lab06.py
@@ -0,0 +1,49 @@
+
+import random
+import time
+
+#----- Version 1 -----#
+
+def pick6():
+    
+    ticket = []
+    
+    for x in range(6):
+        ticket.append(random.randint(1,99))
+        # print(ticket) 
+    return ticket
+
+# print(pick6())
+
+def num_matches(win_tix,tix_n_play):
+    
+    matches = 0
+
+    for i in range(6):
+        if win_tix[i] == tix_n_play[i]:
+            matches += 1
+    return matches
+
+# print(num_matches())
+
+# match_val = {0:0, 1:4, 2:7, 3:100, 4:50000, 5:1000000, 6:25000000}
+
+# num_match = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+
+win_tix = pick6()
+tix_n_play = pick6()
+
+print('\nWelcome to Pick6!')
+print(f'\nYour ticket numbers...')
+time.sleep(1) 
+print(f'\n{tix_n_play}')
+print(f'\nThe winning numbers...')
+time.sleep(1)
+print(f'\n{win_tix}')
+
+
+
+
+
+
+
