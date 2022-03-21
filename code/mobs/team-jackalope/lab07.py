1a213538ed348b398ada07f4a66e703cc6d7fbc5
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-21 14:15:18 -0700

    jackalopes

---
+++ code/mobs/team-jackalope/lab07.py
@@ -0,0 +1,21 @@
+jackalopes = [0,0]
+
+year = 0
+
+# create function
+def jocko_older():
+
+
+while True:
+
+    year += 1
+
+    for index,jackalope in enumerate(jackalopes):
+        jackalopes[index] = jackalope + 1
+
+
+
+
+
+    
+
