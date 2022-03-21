1ed1c631dd52128325778f217b709941e3ce455d
asddataking <asddataking@outlook.com>
2022-03-21 18:03:33 -0400

    Lab04 updates still working

--- code/dan/lab04.py
+++ code/dan/lab04.py
@@ -1,5 +1,13 @@
 
 answer = input ("Please choose a number: ") # Requestes user input a number
+new_answer = int(answer)
+
+
+value = int(answer)
+ones = value % 10
+tens = (value % 100) // 10 
+hundreds = value // 100
+print (f"{hundreds} hundreds {tens} tens {ones} ones")
 
 
 # basic dictionary 
@@ -14,6 +22,15 @@ numdict = {
  8 : "eight",
  9 : "nine",
  10 : "ten",
+ 11 : "eleven",
+ 12 : "twelve",
+ 13 : "thirteen",
+ 14 : "fourteen",
+ 15 : "fifteen",
+ 16 : "sixteen",
+ 17 : "seventeen",
+ 18 : "eighteen",
+ 19 : "nineteen",
  20 : "twenty",
  30 : "thirty",
  40 : "fourty" ,  
@@ -25,11 +42,35 @@ numdict = {
  100: "one hundred",
     
 }
-answer = input ("Please choose a number: ") # Requestes user input a number
-new_answer = int(answer)
+
+x = numdict[hundreds]
+y = numdict[tens]
+z = numdict[ones]
+print (x,y,z)
+
 
 # if key == user input then print value of that key. 
 if new_answer in numdict:
         print(numdict[new_answer])
 else:
         print ("Out of dictionary bounds")
+        
+        
+        
+        #number text list
+a_list = ['zero', 'one', 'two','three', 'four', "five",'six','seven','eight','nine',]
+
+answer = input ("Please choose a number: ") # Requestes user input a number
+
+value = int(answer)
+
+for char in answer:
+    char = int(char)
+    print(a_list[char])
+    
+value = int(answer)
+ones = value % 10
+tens = (value % 100) // 10 #not right
+hundreds = value // 100
+print (f"{hundreds} hundreds {tens} tens {ones} ones")
+        
