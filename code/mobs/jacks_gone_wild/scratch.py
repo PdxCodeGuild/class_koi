35cde80bd386e04c65716e10874cb7ad8540659f
ShaineWarren <mwarren86@gmail.com>
2022-03-21 17:19:11 -0400

    started lab07

---
+++ code/mobs/jacks_gone_wild/scratch.py
@@ -0,0 +1,15 @@
+
+def aging(input):
+    for x in range(len(input)):
+
+
+
+population = 0
+
+while population < 1000:
+
+
+
+
+# each index is an age group index 0= age 0 and each interger = population
+jackalope  = [2,0,0,0,0,0,0,0,0,0]
