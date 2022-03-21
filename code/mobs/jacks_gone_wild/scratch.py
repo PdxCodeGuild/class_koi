b1e0e25397e93ca9caa8c00d37e5222d7ca2d8f0
MattDWalsh <matthew.david.walsh@gmail.com>
2022-03-21 14:45:35 -0700

    updated lab07.py

--- code/mobs/jacks_gone_wild/scratch.py
+++ code/mobs/jacks_gone_wild/scratch.py
@@ -1,19 +1,59 @@
+"""
+each iteration of aging function = 1 year
+    iterate through each age group
+        subtract the current population from that age
+        add that population to the next age (in temp variable)
+
+        if old enough to breed, add 2 per breeding pair to year 0
+
+        if too old, kill em 
+    
+
+run function in while loop until population is >= 1000
+    iterate number of years within loop
+
+"""
+
+
+
 
 def aging(input):
+    # population placeholder
     temp_jack = input
-    
+    # iterate through each index position of input
     for x in range(len(input)):
+        if x == 9:
+            temp_jack[x] -= input[x]
+        else:
+            temp_jack[x + 1] = input[x]
+            temp_jack[x] -= input[x]
 
-    return population
+    return temp_jack
+
+
+
 
 # each index is an age group index 0= age 0 and each interger = population
 jackalope  = [2,0,0,0,0,0,0,0,0,0]
 
+# generate variables to track population and number of years
 population = 0
 years = 0
+
+# run loop until population is at or above 1000
 while population < 1000:
-    aging(jackalope)
+    # update jackalope variable with function output
+    jackalope = aging(jackalope)
+    # iterate year variable
     years += 1
+    population = sum(jackalope)
+
+
+    # 
+    print(jackalope)
+
+
+
 
 
 
