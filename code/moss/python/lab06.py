e14bb2715208bf8494107fe6393bc6ee711a81bc
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-21 23:55:21 -0700

    Lab06 Pick6

--- code/moss/python/lab06.py
+++ code/moss/python/lab06.py
@@ -13,7 +13,7 @@ def pick6():
         # print(ticket) 
     return ticket
 
-# print(pick6())
+# pick6()
 
 def num_matches(win_tix,tix_n_play):
     
@@ -24,14 +24,21 @@ def num_matches(win_tix,tix_n_play):
             matches += 1
     return matches
 
-# print(num_matches())
+# num_matches()
 
 # match_val = {0:0, 1:4, 2:7, 3:100, 4:50000, 5:1000000, 6:25000000}
 
 # num_match = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
 
 win_tix = pick6()
-tix_n_play = pick6()
+
+
+earned = 0
+balance = 0
+expenses = 0
+
+for i in range(int(1000)):
+    tix_n_play = pick6()
 
 print('\nWelcome to Pick6!')
 print(f'\nYour ticket numbers...')
