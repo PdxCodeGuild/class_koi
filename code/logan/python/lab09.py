166417fdad713fe9d14e05f7740c8e5c39d1c10d
logandouglass <logandouglass@gmail.com>
2022-03-22 15:29:03 -0400

    still work in progress

--- code/logan/python/lab09.py
+++ code/logan/python/lab09.py
@@ -1,13 +1,23 @@
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peaks(input):
-    peaks = []
+    peak_indices = []
     for i in range(len(input)):
-        if i != 0 and i != -1:
+        if i != 0 and i != (len(input) -1):
             if input[i] > input[i - 1] and input[i] > input[i + 1]:
-                peaks.append(input[i])
-    return peaks
+                peak_indices.append(i)
+    return peak_indices
 
+def valleys(input):
+    valley_indices = []
+
+
+## Tests
+
+# print(len(data))
 # print(range(len(data)))
+
+# for item in range(len(data)):
+#     print(item)
 # print(peaks(data))
-print(len(data))
+# print(len(data))
