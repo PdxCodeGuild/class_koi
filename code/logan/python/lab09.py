d4554c16046ce316ac93bb07b701724edad8e164
logandouglass <logandouglass@gmail.com>
2022-03-22 16:53:03 -0400

    version 1

--- code/logan/python/lab09.py
+++ code/logan/python/lab09.py
@@ -1,6 +1,13 @@
-data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+##Lab09V1
+##Code
+
+## Uncomment data if you want to pass the data set as a test.
+## There are helpful print statements for testing as the
+## bottom of the page
+# data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peaks(input):
+# Pulls out the peaks in a list, inputs them in a list
     peak_indices = []
     for i in range(len(input)):
         if i != 0 and i != (len(input) -1):
@@ -9,15 +16,25 @@ def peaks(input):
     return peak_indices
 
 def valleys(input):
+# Pulls out the valleys in a list, outputs them in a list
     valley_indices = []
+    for i in range(len(input)):
+        if i != 0 and i != (len(input) -1):
+            if input[i] < input[i - 1] and input[i] < input[i + 1]:
+                valley_indices.append(i)
+    return valley_indices
 
+def peaks_and_valleys(input):
+# Outputs a list of all peaks and valleys in a list in the order they appear in the input
+    output = []
+    for valley in valleys(input):
+        output.append(valley)
+    for peak in peaks(input):
+        output.append(peak)
+    output.sort()
+    return output
 
 ## Tests
-
-# print(len(data))
-# print(range(len(data)))
-
-# for item in range(len(data)):
-#     print(item)
 # print(peaks(data))
-# print(len(data))
+# print(valleys(data))
+# print(peaks_and_valleys(data))
