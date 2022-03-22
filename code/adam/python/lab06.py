981653c7a5ba26e005a1d28af3fa186043c82d22
AdamFraley <adamfraley@gmail.com>
2022-03-22 09:43:29 -0700

    lab06 working on while loop

--- code/adam/python/lab06.py
+++ code/adam/python/lab06.py
@@ -15,25 +15,41 @@ def pick6():
     temp_ticket = nums[:7]
     return temp_ticket
 
-winning_ticket = pick6() # winning_ticket = pick6(nums)
-ticket = pick6() # purchased ticket
-
 # function to check winning ticket and purchased ticket
 def num_matches(winning_ticket, ticket):
-    matches = 0
+    matches_temp = 0
     for i in range(6):
         if winning_ticket[i] == ticket[i]:
-            matches += 1
-    return matches
+            matches_temp += 1
+    return matches_temp
+
+winnings_dict = {
+    0 : 0,
+    1 : 4,
+    2 : 7,
+    3 : 100,
+    4 : 50000,
+    5 : 1000000,
+    6 : 25000000,
+}
 
 # winning_ticket = [1,2,3,4,5,6]
 # ticket = [1,2,3,4,5,6]
+# print(winning_ticket)
+# print(ticket)
+# print (num_matches(winning_ticket, ticket))
+# matches = num_matches(winning_ticket, ticket)
+# print(f'${winnings_dict[matches]}')
 
-print(winning_ticket)
-print(ticket)
-print (num_matches(winning_ticket, ticket))
-
-
-
-
-
+i = 0
+while i < 10:
+    i =+ 1
+    cost = 0
+    winning_ticket = pick6() # winning_ticket = pick6(nums)
+    ticket = pick6() # purchased ticket
+    matches = num_matches(winning_ticket, ticket)
+    winnings = winnings_dict[matches]
+    cost = cost - 2 + winnings
+    print(winning_ticket)
+    print(ticket)
+    print(matches)
