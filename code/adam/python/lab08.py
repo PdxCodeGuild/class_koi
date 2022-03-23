bde94495c6c7ddc723b2380a8ee9f8b41422717d
AdamFraley <adamfraley@gmail.com>
2022-03-23 12:47:18 -0700

    completed lab08 credit card validation function

--- code/adam/python/lab08.py
+++ code/adam/python/lab08.py
@@ -1,41 +1,35 @@
-# Lab 08 - Credit Card Validation
+# Lab 08 - Credit Card Validation function
+   
+# function to validate credit card input string
+def credit_card_validation(cc_num_str):
+    # Convert the input string of numbers into a list of ints
+    credit_card_list = []
+    for num in credit_card_str:
+        credit_card_list.append(int(num))
+    # Slice off the last digit. That is the check digit.
+    check_digit = credit_card_list.pop(-1)
+    # Reverse the digits.
+    credit_card_list.reverse()
+    # Double every other element in the reversed list.
+    for i, num in enumerate(credit_card_list):
+        if i % 2 == 0:
+            credit_card_list[i] *= 2
+    # Subtract nine from numbers over nine.
+    for i, num in enumerate(credit_card_list):
+        if credit_card_list[i] > 9:
+            credit_card_list[i] -= 9
+    # Sum all values.
+    sum_of_list = sum(credit_card_list)
+    # Take the second digit of that sum.
+    second_digit = int(str(sum_of_list)[1])
+    # If that matches the check digit, the whole card number is valid.
+    if second_digit == check_digit:
+        print('Credit Card number is Valid!')
+    else:
+        print('Credit Card number is In-Valid')
 
 # request credit card number from user
 credit_card_str = input("Enter the 16-digit credit card number: ")
-    
-# function to validate credit card input string
-# def credit_card_validation():
 
-# Convert the input string of numbers into a list of ints
-credit_card_list = []
-for num in credit_card_str:
-    credit_card_list.append(int(num))
-
-# Slice off the last digit. That is the check digit.
-check_digit = credit_card_list.pop(-1)
-
-# Reverse the digits.
-credit_card_list.reverse()
-
-# Double every other element in the reversed list.
-# i = 0
-for i in credit_card_list:
-    credit_card_list[i] = credit_card_list[i] + credit_card_list[i]
-# credit_card_list[0] = credit_card_list[0] * 2
-# credit_card_list[2] = credit_card_list[2] * 2
-# credit_card_list[4] = credit_card_list[4] * 2
-# credit_card_list[6] = credit_card_list[6] * 2
-# credit_card_list[8] = credit_card_list[8] * 2
-# credit_card_list[10] = credit_card_list[10] * 2
-# credit_card_list[12] = credit_card_list[12] * 2
-# credit_card_list[14] = credit_card_list[14] * 2
-
-# Subtract nine from numbers over nine.
-# if credit_card_list[0] > 9:
-#     credit_card_list[0] = credit_card_list[0] - 9
-
-
-# Sum all values.
-# Take the second digit of that sum.
-# If that matches the check digit, the whole card number is valid.
-print(credit_card_list)
+# call function and run input string of credit card number
+credit_card_validation(credit_card_str)
