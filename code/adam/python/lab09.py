cd37a49b1da7cdc002ec2309d92db3ddd81565eb
AdamFraley <adamfraley@gmail.com>
2022-03-23 14:34:59 -0700

    lab09 peak function save

--- code/adam/python/lab09.py
+++ code/adam/python/lab09.py
@@ -4,8 +4,14 @@ data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
 print(len(data))
 # define function 'peaks' 
 # Returns the indices of peaks. A peak has a lower number on both the left and the right.
-for i, num in enumerate(data):
-    if 
+peaks = []
+for i in range(1, len(data)-1):
+    if data[i] > data[i-1] and data[i] > data[i+1]:
+        peaks.append(i)
+print(peaks)
+
+
+    
 # define function 'valleys'
 # Returns the indices of 'valleys'. A valley is a number with a higher number on both the left and the right.
 # uses the above two functions to compile a single list of the peaks and valleys 
