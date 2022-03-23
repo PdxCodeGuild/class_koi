abfc2534c77d8327649ec958e268c9a0655129bb
AdamFraley <adamfraley@gmail.com>
2022-03-23 13:37:36 -0700

    starting lab09

---
+++ code/adam/python/lab09.py
@@ -0,0 +1,12 @@
+# Lab 09 - Peaks and Valleys
+
+data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
+print(len(data))
+# define function 'peaks' 
+# Returns the indices of peaks. A peak has a lower number on both the left and the right.
+for i, num in enumerate(data):
+    if 
+# define function 'valleys'
+# Returns the indices of 'valleys'. A valley is a number with a higher number on both the left and the right.
+# uses the above two functions to compile a single list of the peaks and valleys 
+# in order of appearance in the original data.
