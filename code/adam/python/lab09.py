53b2f3eea8b52399c3eecf751e498399f6c0ede5
AdamFraley <adamfraley@gmail.com>
2022-03-23 15:22:18 -0700

    lab 09 with the 3 functions - peaks, valleys, peaks_and_valleys

--- code/adam/python/lab09.py
+++ code/adam/python/lab09.py
@@ -2,27 +2,34 @@
 
 data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
 # define function 'peaks' 
-# Returns the indices of peaks. A peak has a lower number on both the left and the right.
+# -Returns the indices of peaks. A peak has a lower number on both the left and the right.
 def peaks (data):
     peaks_index_list = []
     for i in range(1, len(data)-1):
         if data[i] > data[i-1] and data[i] > data[i+1]:
             peaks_index_list.append(i)
-    print(peaks_index_list)
+    return peaks_index_list
 
-peaks(data)
+print(peaks(data))
 
 # define function 'valleys'
-# Returns the indices of 'valleys'. A valley is a number with a higher number on both the left and the right.
-def valleys (data):
+# -Returns the indices of 'valleys'. A valley is a number with a higher number on both the left and the right.
+def valleys(data):
     valleys_index_list = []
     for i in range(1, len(data)-1):
         if data[i] < data[i-1] and data[i] < data[i+1]:
             valleys_index_list.append(i)
-    print(valleys_index_list)
+    return valleys_index_list
 
-valleys(data)
+print(valleys(data))
 
+# define function peaks_and_valleys
+# -uses the above two functions to compile a single list of the peaks and valleys 
+# -in order of appearance in the original data.
+def peaks_and_valleys():
+    peaks_and_valleys_list = peaks(data) + valleys(data)
+    peaks_and_valleys_list.sort()
+    return peaks_and_valleys_list
 
-# uses the above two functions to compile a single list of the peaks and valleys 
-# in order of appearance in the original data.
+print(peaks_and_valleys())
+
