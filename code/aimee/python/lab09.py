9085488f8267407a444fe26cc1ea8953713a8c7f
Aimee <aimeenyy@gmail.com>
2022-03-23 14:20:44 -0700

    Completed ver1 lab09 peaks and valleys

--- code/aimee/python/lab09.py
+++ code/aimee/python/lab09.py
@@ -1,22 +1,6 @@
 # Lab 09, Peaks and Valleys ---- #
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-# indices 0 - 20 
-
-# def peaks(data): # return the index position of number with lower value on either side
-#     #peak_total = []
-#     for index, elem in enumerate(data):
-#         if data[index+1] < elem and data[index-1] < elem:
-#             # peak_total.append(index)
-#             return index
-#     return index # is returning index 6, should be 6 and 14
-#         # else:
-#         #     break # gives none
-
-# # def valleys():
-# #     return True 
-
-#print(peaks(data)) # list index is out of range
 
 def peaks(data):
     peak_total = []
@@ -27,6 +11,7 @@ def peaks(data):
         
     return peak_total
 
+
 def valleys(data):
     valley_total = []
     for i in range(len(data)): 
@@ -36,12 +21,8 @@ def valleys(data):
         
     return valley_total
 
-# print(peaks(data)) #[6, 14]
-# print(valleys(data)) #[9, 17]
-
 
 def peaks_and_valleys(data):
-    # peaks_valleys = []
     p = peaks(data) # variable equaling function
     v = valleys(data)
     peaks_valleys = p + v
