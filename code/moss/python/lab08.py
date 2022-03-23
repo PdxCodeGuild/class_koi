790bac689b1092b25fa92e74074e957d7a79f39f
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-23 14:11:37 -0700

    To be continued...

--- code/moss/python/lab08.py
+++ code/moss/python/lab08.py
@@ -1,13 +1,35 @@
 print('\nWelcome to the Credit Card Validator.')
 
-card_num = input('\nPlease enter, the sixteen digit credit card number to validate:')
+# card_num = input('''\nPlease enter, 
+# the sixteen digit credit card number to validate:''')
 
-cred_card_test = '4 5 5 6 7 3 7 5 8 6 8 9 9 8 5 5'
+cred_card_test = '4556737586899855'
 
-def cred_card_valid(card_num):
-    
-    list_ints = list(card_num)
-    print(list_ints)
+def cred_card_valid(cred_card_test):
+    #1
+    list_ints = list(cred_card_test)
+    for i in range(len(list_ints)):
+        list_ints[i] = int(list_ints[i])
+    print(f'\n1.{list_ints}.')
+    #2
+    chk_digit = list_ints.pop()
+    # print(chk_digit)
+    print(f'\n2.{list_ints}.')
+    #3
+    reversed_digit = list(reversed(list_ints))
+    print(f'\n3.{reversed_digit}.')
+    #4
+    for i in range(0,len(reversed_digit),2):
+        reversed_digit[i] *=2
+    print(f'\n4.{reversed_digit}.')
 
-cred_card_valid(card_num)
+
+
+
+
+
+
+
+
+cred_card_valid(cred_card_test)
 
