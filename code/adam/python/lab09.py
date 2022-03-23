276d22e5de15743aa0863a4098efd25481ce5032
AdamFraley <adamfraley@gmail.com>
2022-03-23 14:55:41 -0700

    lab09 added valley function

--- code/adam/python/lab09.py
+++ code/adam/python/lab09.py
@@ -1,18 +1,28 @@
 # Lab 09 - Peaks and Valleys
 
 data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
-print(len(data))
 # define function 'peaks' 
 # Returns the indices of peaks. A peak has a lower number on both the left and the right.
-peaks = []
-for i in range(1, len(data)-1):
-    if data[i] > data[i-1] and data[i] > data[i+1]:
-        peaks.append(i)
-print(peaks)
+def peaks (data):
+    peaks_index_list = []
+    for i in range(1, len(data)-1):
+        if data[i] > data[i-1] and data[i] > data[i+1]:
+            peaks_index_list.append(i)
+    print(peaks_index_list)
 
+peaks(data)
 
-    
 # define function 'valleys'
 # Returns the indices of 'valleys'. A valley is a number with a higher number on both the left and the right.
+def valleys (data):
+    valleys_index_list = []
+    for i in range(1, len(data)-1):
+        if data[i] < data[i-1] and data[i] < data[i+1]:
+            valleys_index_list.append(i)
+    print(valleys_index_list)
+
+valleys(data)
+
+
 # uses the above two functions to compile a single list of the peaks and valleys 
 # in order of appearance in the original data.
