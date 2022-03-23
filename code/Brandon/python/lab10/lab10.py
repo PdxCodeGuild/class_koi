8a1d45d5c12856b011fd071277a7a66525387f66
Brandon Sarazin <bsarazin1@gmail.com>
2022-03-23 18:59:51 -0400

    lab10 complete

---
+++ code/Brandon/python/lab10/lab10.py
@@ -0,0 +1,21 @@
+# Write a program that prompts the user for a string, and encodes it with ROT13. For each character, find the corresponding character, add it to an output string. Notice that there are 26 letters in the English language, so encryption is the same as decryption.
+
+#Version 1
+alphachar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '']
+rot13char = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', ' ', '']
+
+
+user_char = input('Please input the string you would like to convert rot13: ')
+outputrot = ''
+outputalpha = ''
+user_char = user_char.lower()
+
+for a in user_char:
+    index = []
+    index = alphachar.index(a)
+    rot13 = rot13char.index(a)
+    rot13 = rot13char[index]
+    outputrot += rot13
+    outputalpha += a
+
+print(f'User input: {outputalpha} \nRot13 output: {outputrot}')
