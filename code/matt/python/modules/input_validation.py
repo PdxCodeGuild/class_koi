bc4bdb7f623e5292dcfd5015030fbf2d61f661f0
MattDWalsh <matthew.david.walsh@gmail.com>
2022-03-23 16:48:21 -0700

    added valid_flt function to input_validation.py (incomplete)

--- code/matt/python/modules/input_validation.py
+++ code/matt/python/modules/input_validation.py
@@ -12,6 +12,7 @@ def valid_str(prompt,choices,error=None,case_sensitive=True,return_case=None):
     -   (optional) error is the error message displayed if invalid
     -   (optional) case_sensitive uses bool to validate with/without case-sensitivity (default is case-sensitive)
     -   (optional) return_case allows the string to be returned with built-in string case methods
+    Use None as needed for default functionality of optional arguments
     """
     # list of built-in string case methods
     cases = ['upper','lower','title','capitalize']
@@ -48,6 +49,7 @@ def valid_int(prompt,error=None,min=None,max=None):
     -   (optional) error is the error message displayed if invalid
     -   (optional) min is the smallest integer accepted (inclusive)
     -   (optional) max is the largest integer accepted (inclusive)
+    Use None as needed for default functionality of optional arguments
     """
     # continue looping until valid input is given
     while True:
@@ -61,9 +63,10 @@ def valid_int(prompt,error=None,min=None,max=None):
                 min = user_input
             if max is None:
                 max = user_input
-            # return input as integer if it is within range
+            # return input as integer if within range
             if user_input >= min and user_input <= max:
                 return user_input
+            # display min/max error message if outside range
             elif user_input > max:
                 print(f'{max} is the highest accepted value.')
             elif user_input < min:
@@ -77,17 +80,47 @@ def valid_int(prompt,error=None,min=None,max=None):
             else:
                 print(error)
 
-def valid_flt(prompt,error=None,min=None,max=None,round=None):
+def valid_flt(prompt,error=None,min=None,max=None,rounded=None):
     """
     Validate float input by attempting to convert string to float
     -   (required) prompt is the string that will prompt the user for input
     -   (optional) error is the error message displayed if invalid
     -   (optional) min is the smallest float accepted (inclusive)
     -   (optional) max is the largest float accepted (inclusive)
-    -   (optional) round is the number of decimal points to round the result to
+    -   (optional) rounded is the number of decimal points to round the result to
+    Use None as needed for default functionality of optional arguments
     """
-    print('not implemented')
-    pass
+    # continue looping until valid input is given
+    while True:
+        # prompt for input
+        user_input = input(prompt)
+        # attempt to convert to integer
+        try:
+            user_input = float(user_input)
+            # set min and/or max to match user_input if unspecified
+            if min is None:
+                min = user_input
+            if max is None:
+                max = user_input
+            # return input as rounded float if within range and round specified
+            if user_input >= min and user_input <= max and rounded is not None:
+                return round(user_input,rounded)
+            # return input as rounded float if within range
+            if user_input >= min and user_input <= max:
+                return user_input
+            # display min/max error message if outside range
+            elif user_input > max:
+                print(f'{max} is the highest accepted value.')
+            elif user_input < min:
+                print(f'{min} is the lowest accepted value.')
+        # display error if conversion fails
+        except:
+            # display default error message if not specified
+            if error is None:
+                print(f'{user_input} is not a number.')
+            # display error message if specified
+            else:
+                print(error)
 
 def valid_lst(prompt,error=None,min_lenth=None,max_length=None,delineator=','):
     """
@@ -97,6 +130,7 @@ def valid_lst(prompt,error=None,min_lenth=None,max_length=None,delineator=','):
     -   (optional) min_length is the minimum number of items in the list
     -   (optional) max_length is the maximum number of items in the list
     -   (optional) delineator is the seperator to look for
+    Use None as needed for default functionality of optional arguments
     """
     print('not implemented')
     pass
