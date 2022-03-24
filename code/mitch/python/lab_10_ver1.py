7d43be0b681be4c46cbc55bec206c5741c8c0b93
chapman311 <fixerchapman@gmail.com>
2022-03-24 11:47:59 -0700

    lab 10 version 1, added support for encryption/decryption choice and user input with dynamic output statements

--- code/mitch/python/lab_10_ver1.py
+++ code/mitch/python/lab_10_ver1.py
@@ -3,35 +3,41 @@
 # Mitch Chapman
 
 
-english = "abcdefghijklmnopqrstuvwxyz"
-rot_13 = "nopqrstuvwxyzabcdefghijklm"
+def translate_to_rot_13(input_str:str, encryption_or_decryption:str):
+    
+    english_lowercase = "abcdefghijklmnopqrstuvwxyz"
+    english_uppercase = english_lowercase.upper()
+    english_full = english_lowercase + english_uppercase
 
-
-def translate_to_rot_13(input_str:str):
-    return(input_str.lower().translate(input_str.lower().maketrans(english, rot_13)))
-
-print(translate_to_rot_13("abcd"))
-
-
-
-
- 
-
-
-
-
-
-########This is the code I am referencing. I wrote this for a codewars challange.##########
-
-# def DNA_strand(dna):
-#     return(dna.upper().translate(dna.upper().maketrans('ATGC', 'TACG')))   
-
-# print(DNA_strand("aaa"))
+    rot_13_lowercase = "nopqrstuvwxyzabcdefghijklm"
+    rot_13_uppercase = rot_13_lowercase.upper()
+    rot_13_full = rot_13_lowercase + rot_13_uppercase
+    
+    print(f"\n{'Encryption' if encryption_or_decryption == 'encrypt' else 'Decryption'} has been appplied to '{input_str}':")
+    
+    
+    return(input_str.translate(input_str.maketrans(english_full, rot_13_full)))
 
 
 
-#########More info here:
-# https://www.w3schools.com/python/ref_string_maketrans.asp
+
+print("\nThis is the 'ROT-13' encryption machine!")
+while True:
+    user_encryption_selection = input("Please indicate weather you would like to encrypt english to ROT-13 or from ROT-13 to english.\n(enter 'encrypt' or 'decrypt'): ").lower()
+    if user_encryption_selection != "encrypt" and user_encryption_selection != "decrypt":
+        print("This is an invalid selection. Try again.")
+    else:
+        break
+
+
+user_input_phrase = input(f"Please enter a phrase you would like to have {'encrypted' if user_encryption_selection == 'encrypt' else 'decrypted'} by the ROT-13 encription machine:\n")
+print(translate_to_rot_13(user_input_phrase, user_encryption_selection))
+
+
+
+
+
+
 
 
 
