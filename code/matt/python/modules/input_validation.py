eb61445e790598a8bed1629cb5bd1ee135352a8d
MattDWalsh <matthew.david.walsh@gmail.com>
2022-03-24 00:22:48 -0700

    renamed valid_lst to valid_list and improved min/max check, added valid_dict (incomplete)

--- code/matt/python/modules/input_validation.py
+++ code/matt/python/modules/input_validation.py
@@ -42,15 +42,18 @@ def valid_str(prompt,choices,error=None,case_sensitive=True,return_case=None):
         else:
             print(error)
 
-def valid_int(prompt,error=None,min=None,max=None):
+def valid_int(prompt=None,error=None,min=None,max=None):
     """
     Prompt for and validate integer input by attempting to convert string to integer
-    -   (required) prompt is the string that will prompt the user for input
+    -   (optional) prompt is the string that will prompt the user for input
     -   (optional) error is the error message displayed if invalid
     -   (optional) min is the smallest integer accepted (inclusive)
     -   (optional) max is the largest integer accepted (inclusive)
     -   Use None as needed for default functionality of optional arguments
     """
+    # set input prompt if blank
+    if prompt is None:
+        prompt = 'Please enter a number: '
     # continue looping until valid input is given
     while True:
         # prompt for input
@@ -80,16 +83,19 @@ def valid_int(prompt,error=None,min=None,max=None):
             else:
                 print(error)
 
-def valid_flt(prompt,error=None,min=None,max=None,rounded=None):
+def valid_flt(prompt=None,error=None,min=None,max=None,rounded=None):
     """
     Prompt for and validate float input by attempting to convert string to float
-    -   (required) prompt is the string that will prompt the user for input
+    -   (optional) prompt is the string that will prompt the user for input
     -   (optional) error is the error message displayed if invalid
     -   (optional) min is the smallest float accepted (inclusive)
     -   (optional) max is the largest float accepted (inclusive)
     -   (optional) rounded is the number of decimal points to round the result to
     -   Use None as needed for default functionality of optional arguments
     """
+    # set input prompt if blank
+    if prompt is None:
+        prompt = 'Please enter a number: '
     # continue looping until valid input is given
     while True:
         # prompt for input
@@ -140,10 +146,10 @@ def valid_int_flt_lite(num,valid_type=None):
     except:
         return False
 
-def valid_lst(prompt,error=None,item_type=None,min_length=None,max_length=None,delineator=','):
+def valid_list(prompt=None,error=None,item_type=None,min_length=None,max_length=None,delineator=','):
     """
-    Validate list input by attempting to convert string to list
-    -   (required) prompt is the string that will prompt the user for input
+    Validate and convert list input by attempting to convert string to list
+    -   (optional) prompt is the string that will prompt the user for input
     -   (optional) error is the error message displayed if invalid
     -   (optional) item_type is the type to convert list items to (default will return list of strings)
     -   (optional) min_length is the minimum number of items in the list
@@ -151,6 +157,9 @@ def valid_lst(prompt,error=None,item_type=None,min_length=None,max_length=None,d
     -   (optional) delineator is the seperator to look for
     -   Use None as needed for default functionality of optional arguments
     """
+    # set input prompt if blank
+    if prompt is None:
+        prompt = f'Please enter a list separated by "{delineator}": '
     # continue looping until valid input is given
     while True:
         # set/reset int/flt validation error flag
@@ -178,30 +187,50 @@ def valid_lst(prompt,error=None,item_type=None,min_length=None,max_length=None,d
         # reset if error triggered above
         if int_flt_error:
             continue
+        # return list if min/max length not specified
+        if min_length is None and max_length is None:
+            return user_input
         # setup variables for min/max check
         item_items = 'item' if len(user_input) == 1 else 'items'
         too_short = f'You entered {len(user_input)} {item_items}, but the minimum is {min_length}.'
         too_long = f'You entered {len(user_input)} {item_items}, but the maximum is {max_length}.'
-        # return list if min/max length not specified
-        if min_length is None and max_length is None:
+        if min_length is None:
+            min_length = len(user_input)
+        if max_length is None:
+            max_length = len(user_input)
+        # check against min/max length
+        if len(user_input) < min_length or len(user_input) > max_length:
+            if len(user_input) < min_length:
+                print(too_short)
+            if len(user_input) > max_length:
+                print(too_long)
+        else:
             return user_input
-        # check against min/max length if both are specified
-        elif min_length is not None and max_length is not None:
-            if len(user_input) < min_length:
-                print(too_short)
-            elif len(user_input) > max_length:
-                print(too_long)
-            else:
-                return user_input
-        # check against min length if only it is specified
-        elif max_length is None:
-            if len(user_input) < min_length:
-                print(too_short)
-            else:
-                return user_input
-        # check against max length if only it is specified
-        elif min_length is None:
-            if len(user_input) < max_length:
-                print(too_long)
-            else:
-                return user_input
+
+def valid_dict(prompt=None,error=None,num_dicts=1,key_type=None,value_type=None,key_value_split=':',pair_split=','):
+    """
+    Validate and convert dictionary input by attempting to convert string to dictionary
+    Supports construction of list of dictionaries
+    -   (optional) prompt is the string that will prompt the user for input
+    -   (optional) error is the error message displayed if invalid
+    -   (optional) num_dicts is the number of dictionaries you would like to create within a list (default is 1, not in a list)
+    -   (optional) key_type is the type to convert keys to
+    -   (optional) value_type is the type to convert values to
+    -   (optional) key_value_split is the delineator between keys and values
+    -   (optional) pair_split is the delineator between pairs
+    """
+    # variables to modify dictionary prompts and loop specified number of times
+    first_loop = True
+    num_loops = num_dicts
+    # loop number of times specified in num_dicts
+    while num_loops > 0:
+        num_loops -= 1
+        # dictionary creation                           ##########
+        print('This function is not yet ready.')        ##########
+        #
+        if prompt is None:
+            user_input = input(f'Please enter {"a" if num_dicts == 1 else "the first" if first_loop else "the last" if num_loops == 0 else "the next"} dictionary: ')
+        else:
+            user_input = input(prompt)
+        first_loop = False
+    pass
