c9f0734ce8b8dd16f791ab9987f89cc3362fb90a
logandouglass <logandouglass@gmail.com>
2022-03-24 15:12:14 -0400

    work in progress

---
+++ code/logan/python/lab10v2.py
@@ -0,0 +1,13 @@
+standard_s = "abcdefghijklmnopqrstuvwxyz"
+
+message = input("\nInput a message to be encrypted to ROT13:\n")
+roations = input("\nInput a number of rotations:\n")
+
+def encryptor(string, number):
+    string_lower = string.lower()
+    encoded_string = ""
+    for char in string_lower:
+        if char in standard_s:
+            i = standard_s.index(char)
+            imod = i + number
+            if 
