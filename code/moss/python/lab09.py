4a610ecf56737971820b7507ee4ab111987392f3
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-24 15:56:20 -0700

    To be continued...

--- code/moss/python/lab09.py
+++ code/moss/python/lab09.py
@@ -1,15 +1,31 @@
-
 data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
 
-peak_list = []
-    
-for i in range(len(data)):
-    if i == 0 or i == len(data)-1:
-        
-        print(data)
+def peaks(data):
 
-#def peaks(data):
+    peak_list = []
+   
+    for i in range(len(data)):
+        if i == 0 or i == len(data)-1:
+            pass
+        else:
+            if data[i] > data[i-1] and data[i] > data[i+1]:
+                peak_list.append(i)
+    return peak_list
 
-#def valleys(data):
+def valleys(data):
 
-#def peaks_and_valleys:
+    valleys_list = []
+
+    for i in range(len(data)):
+        if i == 0 or i == len(data)-1:
+            pass
+        else:
+            if data[i] < data[i-1] and data[i] < data[i+1]:
+                valleys_list.append(i)
+    return valleys_list
+
+peaks_data = peaks(data)
+valleys_data = valleys(data)
+
+print(peaks_data)
+print(valleys_data)
