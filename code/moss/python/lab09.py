24773d51b88724a0e85dbdddfc8ff6971080e4b3
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-24 12:22:33 -0700

    Defining Functions

---
+++ code/moss/python/lab09.py
@@ -0,0 +1,15 @@
+
+data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
+
+peak_list = []
+    
+for i in range(len(data)):
+    if i == 0 or i == len(data)-1:
+        
+        print(data)
+
+#def peaks(data):
+
+#def valleys(data):
+
+#def peaks_and_valleys:
