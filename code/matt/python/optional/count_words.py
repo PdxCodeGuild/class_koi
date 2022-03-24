2a49b7541a185e7dd848cf208527a96f188f7a8f
MattDWalsh <matthew.david.walsh@gmail.com>
2022-03-24 16:56:22 -0700

    added count_words.py

---
+++ code/matt/python/optional/count_words.py
@@ -0,0 +1,44 @@
+'''
+PDX Code Guild - Class Koi
+Optional Lab - Count Words
+Matt Walsh
+'''
+
+ 
+# Version 1
+
+# import punctuation to strip
+from string import ascii_letters, punctuation
+punctuation += '“”‘’—'
+punctuation = punctuation.replace("'",'')
+punctuation = punctuation.replace("-",'')
+
+# open and read file
+with open('code\matt\python\optional\count_words.txt', 'r', encoding='utf-8') as f:
+    contents = f.read()
+
+# strip all punctuation and convert to lowercase
+translator = str.maketrans('', '', punctuation)
+contents = contents.replace('-',' ')
+words_list = contents.translate(translator).lower().split()
+
+# construct dictionary of unique words
+word_dict = {}
+for word in list(set(words_list)):
+    word_dict[word] = 0
+
+# count number of word instances
+for word in word_dict:
+    word_dict[word] = words_list.count(word)
+
+# define words to ignore then remove from dictionary
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+for word in stop_words:
+    if word in word_dict:
+        word_dict.pop(word)
+
+# word_dict is a dictionary where the key is the word and the value is the count
+words = list(word_dict.items()) # .items() returns a list of tuples
+words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+    print(words[i])
