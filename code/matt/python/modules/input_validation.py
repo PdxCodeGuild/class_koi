c7f9916b376a10acd4da40067d3d069e9fe4d156
MattDWalsh <matthew.david.walsh@gmail.com>
2022-03-23 22:55:41 -0700

    added valid_lst and valid_int_flt_lite

--- code/matt/python/modules/input_validation.py
+++ code/matt/python/modules/input_validation.py
@@ -6,7 +6,7 @@ Matt Walsh
 
 def valid_str(prompt,choices,error=None,case_sensitive=True,return_case=None):
     """
-    Validate string input against a list of choices:
+    Prompt for and validate string input against a list of choices:
     -   (required) prompt is the string that will prompt the user for input
     -   (required) choices is a list of valid choices
     -   (optional) error is the error message displayed if invalid
@@ -44,7 +44,7 @@ def valid_str(prompt,choices,error=None,case_sensitive=True,return_case=None):
 
 def valid_int(prompt,error=None,min=None,max=None):
     """
-    Validate integer input by attempting to convert string to integer
+    Prompt for and validate integer input by attempting to convert string to integer
     -   (required) prompt is the string that will prompt the user for input
     -   (optional) error is the error message displayed if invalid
     -   (optional) min is the smallest integer accepted (inclusive)
@@ -80,9 +80,27 @@ def valid_int(prompt,error=None,min=None,max=None):
             else:
                 print(error)
 
+def valid_int_flt_lite(num,valid_type=None):
+    """
+    Basic integer or float validation
+    -   (required) num is the input number to validate
+    -   (optional) valid_type is int or float (will choose if unspecified)
+    -   Use None as needed for default functionality of optional arguments
+    -   Returns False if invalid
+    """
+    try:
+        if valid_type is int:
+            return int(num)
+        elif valid_type is float:
+            return float(num)
+        else:
+            return int(num) if num % 1 == 0 else float(num)
+    except:
+        return False
+
 def valid_flt(prompt,error=None,min=None,max=None,rounded=None):
     """
-    Validate float input by attempting to convert string to float
+    Prompt for and validate float input by attempting to convert string to float
     -   (required) prompt is the string that will prompt the user for input
     -   (optional) error is the error message displayed if invalid
     -   (optional) min is the smallest float accepted (inclusive)
@@ -122,7 +140,7 @@ def valid_flt(prompt,error=None,min=None,max=None,rounded=None):
             else:
                 print(error)
 
-def valid_lst(prompt,error=None,item_type=None,min_lenth=None,max_length=None,delineator=','):
+def valid_lst(prompt,error=None,item_type=None,min_length=None,max_length=None,delineator=','):
     """
     Validate list input by attempting to convert string to list
     -   (required) prompt is the string that will prompt the user for input
@@ -133,18 +151,57 @@ def valid_lst(prompt,error=None,item_type=None,min_lenth=None,max_length=None,de
     -   (optional) delineator is the seperator to look for
     -   Use None as needed for default functionality of optional arguments
     """
-    
-    # refactor to use functions above for item_type == int/flt
-    # consider how to implement item_type == list (possibly specify number of items in each list)
-
     # continue looping until valid input is given
     while True:
+        # set/reset int/flt validation error flag
+        int_flt_error = False
         # prompt for input
         user_input = input(prompt)
-        user_input = user_input.strip('][').split(',') ########## temporary functionality
-        for i in range(len(user_input)): ########## temporary functionality
-            user_input[i] = user_input[i].strip() ########## temporary functionality
-        break ########## temporary functionality
-    return user_input ########## temporary functionality
-
-# consider adding valid_dict - this is much more complex
+        # strip brackets if they exist and split at delineator
+        user_input = user_input.strip('][').split(delineator)
+        # iterate through items on list and strip leading/trailing whitespace
+        for i in range(len(user_input)):
+            user_input[i] = user_input[i].strip()
+            # convert to int or float if chosen and valid
+            if item_type is int or item_type is float:
+                # display error if validation fails
+                if valid_int_flt_lite(user_input[i],item_type) is False:
+                    if error is None:
+                        print('An error has occurred.')
+                        # set variable to trigger reset
+                        int_flt_error = True
+                        break
+                    else:
+                        print(error)
+                else:
+                    user_input[i] = valid_int_flt_lite(user_input[i],item_type)
+        # reset if error triggered above
+        if int_flt_error:
+            continue
+        # setup variables for min/max check
+        item_items = 'item' if len(user_input) == 1 else 'items'
+        too_short = f'You entered {len(user_input)} {item_items}, but the minimum is {min_length}.'
+        too_long = f'You entered {len(user_input)} {item_items}, but the maximum is {max_length}.'
+        # return list if min/max length not specified
+        if min_length is None and max_length is None:
+            return user_input
+        # check against min/max length if both are specified
+        elif min_length is not None and max_length is not None:
+            if len(user_input) < min_length:
+                print(too_short)
+            elif len(user_input) > max_length:
+                print(too_long)
+            else:
+                return user_input
+        # check against min length if only it is specified
+        elif max_length is None:
+            if len(user_input) < min_length:
+                print(too_short)
+            else:
+                return user_input
+        # check against max length if only it is specified
+        elif min_length is None:
+            if len(user_input) < max_length:
+                print(too_long)
+            else:
+                return user_input
