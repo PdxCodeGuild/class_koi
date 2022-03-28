ee822587c62c0726dc25cb766e9f08baad3b2829
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-28 12:31:43 -0700

    Set-up

---
+++ code/moss/python/lab11.py
@@ -0,0 +1,18 @@
+ari_scale = {
+
+Score  {'ages':     Grade Level
+     1:{'ages': '5-6',    Kindergarten
+     2:{'ages': '6-7',    First Grade
+     3:{'ages': '7-8',    Second Grade
+     4:{'ages': '8-9',    Third Grade
+     5:{'ages': '9-10',    Fourth Grade
+     6:{'ages': '10-11',    Fifth Grade
+     7:{'ages': '11-12',    Sixth Grade
+     8:{'ages': '12-13',    Seventh Grade
+     9:{'ages': '13-14',    Eighth Grade
+    10:{'ages': '14-15',    Ninth Grade
+    11:{'ages': '15-16',    Tenth Grade
+    12:{'ages': '16-17',    Eleventh grade
+    13:{'ages': '17-18',    Twelfth grade
+    14:{'ages': '18-22',    College
+}
