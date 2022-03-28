6c85bf91956be97acfe8cfb0d04cc7d1034ea71c
logandouglass <logandouglass@gmail.com>
2022-03-28 14:53:33 -0400

    Lab 12 complete

--- code/logan/python/lab12.py
+++ code/logan/python/lab12.py
@@ -4,26 +4,17 @@
 # Our empty list, to be filled with dictionaries
 smashers = []
 
-# Open the CSV
+# Other variables
 
-with open('smash.csv', 'r') as f:
-    lines = f.read().split('\n')
+options = ""
+options_list = ["0", "1", "2", "3", "4"]
 
-# Harvest the keys as list items
-keys = lines[0]
-keys = keys.split(",")
+######################################
+# Here are all the necessary functions:
 
-lines.pop(0)
-
-# Make the dictionary
-for line in lines:
-    char_as_list = line.split(",")
-    new_dict = {}
-    for i in range(0, len(keys)):
-        new_dict.update({keys[i]:char_as_list[i]})
-    smashers.append(new_dict)
 
 def create(dict_list, key_list):
+# This is the function that creates a new character in the database.    
     name = input("Give me the character's name...").title()
     franchise = input("Give me the character's franchise...").title()
     up_b = input("Give me the character's up-b...").title()
@@ -38,6 +29,7 @@ def create(dict_list, key_list):
     print("\nCharacter created.")
 
 def retrieve(dict_list):
+# This is the function that retrieves a character's information as a dictionary.
     in_list = False
     name = input("\nWho do you want to look up?...")
     name = name.title()
@@ -48,9 +40,10 @@ def retrieve(dict_list):
             break
 
     if in_list == False:
-        print("\nI'm sorry.  They haven't added that character to the game yet!")
+        print("\nI'm sorry.  We haven't added that character yet!")
 
 def uppdate(dict_list):
+# Updates the data of an existing character in the database.
     in_list = False
     valid_responses = [1,2,3,4,5,6]
     att_list = ["Name","Franchise", "Up-B", "Down-B", "Side-B", "Neutral-B",]
@@ -61,20 +54,27 @@ def uppdate(dict_list):
             in_list = True
             break
     if in_list == False:
-        print("\nI'm sorry.  They haven't added that character to the game yet!")
+        print("\nI'm sorry.  We haven't added that character yet!")
         return
-
-    att = input("\nInput the corresponding digit for the attribute you want to modify: \n\n1:Name\n2:Franchise\n3:Up-B\n4:Down-B\n5:Side-B\n6:Neutral-B\n\n...")
-    att = int(att)
-    if att in valid_responses:
-        att_in = att - 1
-        update = input(f"\nWhat is the new {att_list[att_in]}?...")
-        update = update.title()
-        for dict in dict_list:
-            if dict["Name"] == name:
-                dict.update({att_list[att_in]:update})
+    
+    while True:
+        try:
+            att = input("\nInput the corresponding digit for the value you want to modify: \n\n1:Name\n2:Franchise\n3:Up-B\n4:Down-B\n5:Side-B\n6:Neutral-B\n\n...")
+            att = int(att)
+            if int(att) in valid_responses:
+                break
+        except:
+            print("Invalid selction.  Please select again.")
+    
+    att_in = int(att) - 1
+    update = input(f"\nWhat is the new {att_list[att_in]}?...")
+    update = update.title()
+    for dict in dict_list:
+        if dict["Name"] == name:
+            dict.update({att_list[att_in]:update})
 
 def delete(dict_list):
+# Deletes a character from the database.
     in_list = False
     name = input("\nWho do you want to remove?...")
     name = name.title()
@@ -88,18 +88,53 @@ def delete(dict_list):
     if in_list == False:
         print("\nI'm sorry.  That character is not in the game.")
 
-options = ""
-options_list = ["0", "1", "2", "3", "4"]
+def recompiler(key_list, smash_dict_list):
+# This will recompile the data in a list
+# that can be easily iterated upon to write
+# the updated CSV
+    recompiled_list = []
+    recompiled_list.append(",".join(key_list))
+    for smash_dict in smash_dict_list:
+        char_values = []
+        for i in range (0, len(smash_dict)):
+            char_values.append(smash_dict[keys[i]])
+        recompiled_list.append(",".join(char_values))
+    return  recompiled_list
+
+# Here end the functions
+###########################
+
+# Open the CSV
+
+with open('smash.csv', 'r') as f:
+    lines = f.read().split('\n')
+
+# Harvest the keys as list items
+keys = lines[0]
+keys = keys.split(",")
+
+# removes the keys line from imported list of CSVs
+lines.pop(0)
+
+# Makes dictionaries for each character, saves them in the list "smashers"
+for line in lines:
+    char_as_list = line.split(",")
+    new_dict = {}
+    for i in range(0, len(keys)):
+        new_dict.update({keys[i]:char_as_list[i]})
+    smashers.append(new_dict)
+
+# The main menu REPL:
 
 while options != "0":
     options = input(f"""
-    What would you like to do? (Input a digit below):
-    0 - Quit
-    1 - Create a character
-    2 - Look up a character
-    3 - Update a character
-    4 - Delete a character
-    ...""")
+What would you like to do? (Input a digit below):
+0 - Quit
+1 - Create a character
+2 - Look up a character
+3 - Update a character
+4 - Delete a character
+...""")
     if options == "1":
         create(smashers, keys)
     elif options == "2":
@@ -108,16 +143,22 @@ while options != "0":
         uppdate(smashers)
     elif options == "4":
         delete(smashers)
-    else:
+    elif options != "0":
         print("\nI'm sorry, that's not a valid response.")
-        
+
+# Recompiling data for CSV...
+
+recompiled_data = recompiler(keys,smashers)
+
+# Writing the new file...
+
+with open('smashupdate.csv', 'w') as new_smash_sheet:
+   for line in recompiled_data:
+    new_smash_sheet.write(line + "\n")
+
+#End
+
 print("\nGoodbye!")
-
-
-
-
-    
-
 
 
 ## Debugging
@@ -126,55 +167,8 @@ print("\nGoodbye!")
 # retrieve(smashers)
 # uppdate(smashers)
 # delete(smashers)
-
-
 # print(lines)
 # print(keys)
-print(smashers)
-
-## Scraaaap
-
-# # Make the dictionary
-# for i in range (0, len(lines)):
-#     new_dict = {}
-#     for ii in range(0, len(keys)):
-#         new_dict.update({lines[i]:keys[ii]})
-#     smashers.append(new_dict)
-
-# def delete(dict_list):
-#     in_list = False
-#     name = input("Who do you want to remove?...")
-#     name = name.title()
-#     for dict in dict_list:
-#         if dict["Name"] == name:
-#             in_list = True
-#             dict_list.remove(dict)
-#             print("Character removed.")
-#             break 
-
-#     if in_list == False:
-#         print("I'm sorry.  They haven't added that character to the game yet!")
-
-# while options != "0":
-#     while True:
-#         options = input(f"""
-#         What would you like to do? (Input a digit below):
-#         0 - Quit
-#         1 - Create a character
-#         2 - Look up a character
-#         3 - Update a character
-#         4 - Delete a character
-#         ...""")
-#         if options in options_list:
-#             break
-#         else:
-#             print("\nI'm sorry, that's not a valid response.")
-            
-#         if options == "1":
-#             create(smashers, keys)
-#         elif options == "2":
-#             retrieve(smashers)
-#         elif options == "3":
-#             uppdate(smashers)
-#         elif options == "4":
-#             delete(smashers)
+# print(",".join(keys))
+# print(smashers)
+# print(recompiler(keys,smashers))
