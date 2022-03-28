ccea563c707afd9d6f85f046cca2b2516dbb2bb2
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-28 10:39:30 -0700

    Lab 10 Rot Cipher

--- code/moss/python/lab10.py
+++ code/moss/python/lab10.py
@@ -1,21 +1,23 @@
 print('\nWelcome to ROT Cipher!\n')
 
-user_string = input('Enter a word to encode:')
+user_string = input('Enter a word to encode: ')
 
 user_string_list = list(user_string)
 
-print(user_string_list)
+# print(user_string_list)
 
 output_list = []
 
-# eng_to_rot = {'a':'n','b':'o','c':'p','d':'q','e':'r','f':'s',
-# 'g':'t','h':'u','i':'v','j':'w','k':'x','l':'y','m':'z','o':'b',
-# 'p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j',
-# 'x':'k','y':'l','z':'m',' ':' ',}
+eng_to_rot = {'a':'n','b':'o','c':'p','d':'q','e':'r','f':'s',
+'g':'t','h':'u','i':'v','j':'w','k':'x','l':'y','m':'z','o':'b',
+'p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j',
+'x':'k','y':'l','z':'m',' ':' ',}
 
-# for letter in user_string_list:
-#     output_list.append(eng_to_rot[letter])
+for letter in user_string_list:
+    output_list.append(eng_to_rot[letter])
 
-# output_string = ''.join(output_list)
+# print(output_list)
 
-# print(output_string)
+output_string = ''.join(output_list)
+
+print(f'\nThe word "{user_string}" is encoded to "{output_string}"\n')
