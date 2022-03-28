007fdaa0599a85dcddcd5df2946939562ce3d9a1
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-28 14:05:32 -0700

    Defining 'with'&'def'

--- code/moss/python/lab11.py
+++ code/moss/python/lab11.py
@@ -1,18 +1,29 @@
-ari_scale = {
+import re
+import math
 
-Score  {'ages':     Grade Level
-     1:{'ages': '5-6',    Kindergarten
-     2:{'ages': '6-7',    First Grade
-     3:{'ages': '7-8',    Second Grade
-     4:{'ages': '8-9',    Third Grade
-     5:{'ages': '9-10',    Fourth Grade
-     6:{'ages': '10-11',    Fifth Grade
-     7:{'ages': '11-12',    Sixth Grade
-     8:{'ages': '12-13',    Seventh Grade
-     9:{'ages': '13-14',    Eighth Grade
-    10:{'ages': '14-15',    Ninth Grade
-    11:{'ages': '15-16',    Tenth Grade
-    12:{'ages': '16-17',    Eleventh grade
-    13:{'ages': '17-18',    Twelfth grade
-    14:{'ages': '18-22',    College
-}
+with open('', 'r') as f:
+    contents = f.read()
+
+def ari(contents):
+
+
+
+
+
+ari_scale = {
+     
+     1:{'ages':   '5-6', 'grade_level': 'Kindergarten'},
+     2:{'ages':   '6-7', 'grade_level':    '1st Grade'},
+     3:{'ages':   '7-8', 'grade_level':    '2nd Grade'},
+     4:{'ages':   '8-9', 'grade_level':    '3rd Grade'},
+     5:{'ages':  '9-10', 'grade_level':    '4th Grade'},
+     6:{'ages': '10-11', 'grade_level':    '5th Grade'},
+     7:{'ages': '11-12', 'grade_level':    '6th Grade'},
+     8:{'ages': '12-13', 'grade_level':    '7th Grade'},
+     9:{'ages': '13-14', 'grade_level':    '8th Grade'},
+    10:{'ages': '14-15', 'grade_level':    '9th Grade'},
+    11:{'ages': '15-16', 'grade_level':   '10th Grade'},
+    12:{'ages': '16-17', 'grade_level':   '11th grade'},
+    13:{'ages': '17-18', 'grade_level':   '12th grade'},
+    14:{'ages': '18-22', 'grade_level':      'College'},
+    }
