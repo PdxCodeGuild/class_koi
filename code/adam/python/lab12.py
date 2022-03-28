89a319d1bde305aa4eb040481f133f6f3ac0f8ab
AdamFraley <adamfraley@gmail.com>
2022-03-28 14:28:44 -0700

    lab12 version 1 Contact List

--- code/adam/python/lab12.py
+++ code/adam/python/lab12.py
@@ -1,4 +1,5 @@
 # Lab 12 - Contact List
+# Version 1
 
 import csv
 
@@ -8,35 +9,17 @@ with open('data/contacts.csv','r') as csv_contacts:
 
 headers = lines[0]
 headers = headers.split(',')
-print(headers)
-contact1 = lines[1]
-contact1 = contact1.split(',')
-contact2 = lines[2]
-contact2 = contact2.split(',')
-contact3 = lines[3]
-contact3 = contact3.split(',')
-contact4 = lines[4]
-contact4 = contact4.split(',')
-contact5 = lines[5]
-contact5 = contact5.split(',')
-contact6 = lines[6]
-contact6 = contact6.split(',')
-print(contact1)
-print(contact2)
-print(contact3)
-print(contact4)
-print(contact5)
-print(contact6)
-    
-# headers
-# contact1
-contacts_dict = {}
-# keys = headers
-# values = []
-# for i in keys:
-#     for x in values:
-contacts_dict['name'] = 'jery'
+# print(headers)
 
-print(contacts_dict)
+contacts_list = []
+character_dict = {}
+character_list = []
+for line in lines[1:]:
+    character_dict = {}
+    character_list = line.split(',')
+    for i, key in enumerate(headers):
+        character_dict[key] = character_list[i]
+    contacts_list.append(character_dict)
+print(contacts_list)
 
 
