e26bb274dd24b605f674913a6b5a65111edba4a7
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-03-29 10:38:34 -0700

    Removed csv duplicate

--- code/moss/python/lab12.py
+++ code/moss/python/lab12.py
@@ -1,30 +1,24 @@
-#path_to_folder = 'code/class_koi/code/moss/python/data/'
+path_to_folder = './data/family_location.csv'
 
-with open('family_location.csv', 'r') as file:
+with open(path_to_folder, 'r') as file:
     lines = file.read().split('\n')
-
-print(lines)
 
 rows_list = []
 
 for line in lines:
     rows_list.append(line.split(','))
 
-print('-'*72)
-
-print(rows_list)
-
-print('-'*72)
-
 key_list = rows_list.pop(0)
-
-print(key_list)
-
-print('-'*72)
 
 contact_list = []
 
 for row in rows_list:
     contact_list.append({key_list[0]:row[0],key_list[2]:row[2],key_list[3]:row[3]})
 
+print(lines)
+print('-'*72)
+print(rows_list)
+print('-'*72)
+print(key_list)
+print('-'*72)
 print(contact_list)
