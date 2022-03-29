34af4aca980691113f5865a1e79f47dffb28f5dd
chapman311 <fixerchapman@gmail.com>
2022-03-29 14:14:49 -0700

    lab 14 progress/handoff

--- code/mobs/tictactoe_alpha/lab14.py
+++ code/mobs/tictactoe_alpha/lab14.py
@@ -1,28 +1,91 @@
 '''
 Lab 14: Tic Tac Toe
+
+Mitch Chapman
+Chris Roos
+Matt Walsh
 '''
 
 class Player:
     def __init__(self, name, token):
         self.name = name
         self.token = token
+        
+    def __str__(self):
+        return f"Name: {self.name} --- Token: {self.token}"
 
-    
 
 class Game:
 
     def __init__(self):
         self.board = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']]
 
+
     def __str__(self):
+        board_string = ""
         for line in self.board:
-            print('|'.join(line))
+            board_string += ('|'.join(line)) + "\n"
+        return board_string
+    
+    
+    def move(self, x, y, player):
+        if x not in range(3) or y not in range(3):
+            print("This is not a valid positional argument.")
+            return True
+        
+        elif self.board[y][x] == ' ':
+            self.board[y][x] = player.token
+            return False
+        else:
+            print("This position is already occupied.")
+            return True
+            
+            
+    # def calc_winner(self):
+    #     if self.board[0] == self.board [1] == self.board[2]:
+            
+            
+            
+    def is_full(self):
+        ...
+            
+            
+    def is_game_over(self):
+        ...
+            
+            
+            
             
 game_1 = Game()
 
-# print(game_1)
 
-# print(str(game_1))
+player_1 = Player(name="Chris", token="X")
+player_2 = Player(name="Matt", token="O")
+
+player_list = (player_1, player_2)
+
+while True:
+    for player in player_list:
+        print(f"{player.name}, it is your move. You are placing {player.token}'s.")
+        
+        keep_going = True
+        while keep_going:
+            
+            
+            
+            x = int(input(f"{player.name}, enter the x position you want to go (0, 1, or 2): "))
+            y = int(input(f"{player.name}, enter the y position you want to go (0, 1, or 2): "))
+            keep_going = game_1.move(x=x, y=y, player=player)
+        print(game_1)
+
+
+
+
+
+
+
+
+
 
 
 #01234
