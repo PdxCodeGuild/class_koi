ae796e9e797a9921d24c585c7b4c23886a19af55
Brandon Sarazin <bsarazin1@gmail.com>
2022-03-31 15:32:24 -0400

    defining classes

--- code/mobs/baam/tic_tac_toe.py
+++ code/mobs/baam/tic_tac_toe.py
@@ -20,7 +20,13 @@ class Game:
         print(f'{self.board[2][0]}|{self.board[2][1]}|{self.board[2][2]}')
 
     def move(self,x,y,player):
-        pass
+        self.x = x
+        self.y = y
+        self.player = player
+        if self.board[x][y] == ' ':
+            self.board[x][y] = player1_player
+        else:
+            print('Space already occupied. ')
 
     def calc_winner(self):
         pass
@@ -31,9 +37,26 @@ class Game:
     def is_game_over(self):
         pass
 
-# while True:
+while True:
 
-game = Game()
-game.__str__()
+    game = Game()
+    player = Player()
+    game.__str__()
+
+    print('Welcome to Tic-Tac-Toe!\n')
+
+    player1 = input('Player 1, enter your name: ').title()
+    player2 = input('Player 2, enter your name: ').title()
+
+    player1_player = Player(name = player1, token = 'X')
+    player2_player = Player(name = player2, token = 'O')
+
+    player1_move_x = int(input('Enter the x position to move your token to. '))
+    player1_move_y = int(input('Enter the y position to move your token to. '))
+
+        
+    game.move(player1_move_x, player1_move_y, player1)
+    game.__str__()
+    break
 # table = Game.__str__()
-
+    
