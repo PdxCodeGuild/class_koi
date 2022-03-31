ceffdf40567d9b645177589c4a32dcb5ac6af378
chapman311 <fixerchapman@gmail.com>
2022-03-31 14:13:56 -0700

    python lab 16 complete

--- code/mitch/python/lab16_ver2.py
+++ code/mitch/python/lab16_ver2.py
@@ -5,24 +5,39 @@
 import requests
 import time
 
-search_term = "hipster"
+def print_sleep():
+    time.sleep(0.5)
+    print("...")
+    time.sleep(0.6)
 
-response = requests.get(f'https://icanhazdadjoke.com/search?term={search_term}', headers={'Accept': 'application/json'}).json()
-
-for data in response['results']:
-    print(data['joke'])
-    time.sleep(4)
-
-
-
-print('Welcome to the Dad Joke headquarters!')
+print('\nWelcome to the Dad Joke headquarters!')
 while True:
-    user_search_response = input("Would you like to search for a topic for the Dad Joke ('y' or 'n'): ")
-    if user_search_response not in ['y', 'n']:
-        print('Invalid responce.')
+    time.sleep(1)
+    print("\n-----------------------------------------MAIN MENU-----------------------------------------")
+    user_input = input("Would you like to [s]earch for a topic for the Dad Joke or see a [r]andom dad joke: ")
+    if user_input not in ['s', 'r', 'q']:
+        print("Invalid response. Please enter 's' to search, 'r'for random, or 'q' to quit.")
 
-    elif user_search_response == 'y':
-        ...
-        
-    elif user_search_response == 'n':
-        ...
+    elif user_input == 's':
+        search_term = input("Please enter a search term for realated dad jokes: ").lower()
+        response = requests.get(f'https://icanhazdadjoke.com/search?term={search_term}', headers={'Accept': 'application/json'}).json()
+
+        print(f"\nHere are some jokes related to the phrase '{search_term}':")
+        for data in response['results']:
+            for _ in range(4):
+                print_sleep()
+            print(data['joke'])
+            time.sleep(3)
+            print("😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂")
+    elif user_input == 'r':
+        response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'}).json()
+
+        print("\nDad joke incoming...")
+        for _ in range(4):
+            print_sleep()
+        print(response['joke'])
+        time.sleep(3)
+        print("😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂 😂")
+    
+    elif user_input == 'q':
+        break
