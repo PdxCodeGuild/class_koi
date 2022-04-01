e956f4b2d2d6e7d69e9d37d74c16483708dd77bc
chapman311 <fixerchapman@gmail.com>
2022-04-01 14:11:17 -0700

    python optional lab rain data progress

--- code/mitch/python/rain_data_ver1.py
+++ code/mitch/python/rain_data_ver1.py
@@ -2,6 +2,20 @@
 # Version 1
 # Mitch Chapman
 
+#################
+################
+############### +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##############
+############# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+############  This code does not run as expected. I am leaving it here for now.
+############# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##############
+############### +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+################
+#################
+
+
+
 import datetime
 from email.utils import parsedate, parsedate_to_datetime
 import pandas
@@ -13,9 +27,11 @@ def dd_mmm_yyyy_parser(date):
 
 
 url = "https://or.water.usgs.gov/non-usgs/bes/hayden_island.rain"
-rain_data = pandas.read_fwf(url, skiprows=(0,1,2,3,4,5,6,7,8,10), headers=None, parse_date=["Date"], dayfirst=True)
-print(rain_data)
+df = pandas.read_fwf(url, skiprows=(0,1,2,3,4,5,6,7,8,10), headers=None, parse_date=["Date"], date_parser=dd_mmm_yyyy_parser, dayfirst=True)
 
+df1 = df.groupby(["Date", "Total"]).mean()
+
+print(df1)
 
 
 
