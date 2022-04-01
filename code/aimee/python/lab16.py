225b64bca87efb5c0894dff3288e144ca1e5c9b0
Aimee <aimeenyy@gmail.com>
2022-04-01 16:14:23 -0700

    completed lab 16 dad jokes

--- code/aimee/python/lab16.py
+++ code/aimee/python/lab16.py
@@ -1,21 +1,31 @@
 # LAB 16: Dad Joke API ----------- #
 
-# JSON - JavaScript Object Notation 
-
-# Application Programming Interface
-
 import requests
+import time
+    
+# REPL ------------------------------------- #
 
-response = requests.get('https://api.ipify.org')
-
-
-print(response) # response[200] is a successful response
-print(response.text)
-print(type(response.text))
-
-data = response.json()
-print(data)
-print(type(data)) # <class 'dict'>
-
-print(data['data'])
-print(data['data']['character']['firstname'])
+while True:
+    print('*'*78)
+    print('Welcome to the Dad Joke generator!')
+    command = input(f'''What would you like to do?
+    (r)andom joke | (s)earch for joke | (e)xit
+    Enter your selection: ''')
+    print('\n')  
+    if command == 'r':             
+        response = requests.get('https://icanhazdadjoke.com/?limit=1', headers = {'Accept':'application/json'})
+        joke = response.json()
+        print(f"{joke['joke']}\n")
+    elif command == 's':   
+        search_term = input('Enter a term to find dad jokes: ').lower()
+        response = requests.get('https://icanhazdadjoke.com/search', headers = {'Accept':'application/json'}, params={'term':search_term, 'limit':20}) 
+        data = response.json()
+        results = data['results']
+        for result in results:
+            print(f"\n- {result['joke']}\n")        
+            time.sleep(1.5)          
+    elif command == 'e':
+        print('Goodbye!')
+        break
+    else:
+        print('Please enter a valid selection.')
