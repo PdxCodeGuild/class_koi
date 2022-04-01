8be2c77d854abfc002da38a81c028459e1f1c925
AdamFraley <adamfraley@gmail.com>
2022-04-01 16:42:30 -0700

    completed lab16 - Dad Joke API

--- code/adam/python/lab16.py
+++ code/adam/python/lab16.py
@@ -12,7 +12,8 @@ headers = {'Accept': 'application/json'}
 response = requests.get('http://icanhazdadjoke.com/', headers=headers)
 dadjoke_data = response.json()
 dadjoke_joke = dadjoke_data['joke']
-print(dadjoke_joke)
+print(f'Here is a random Dad joke:\n ')
+print(f'{dadjoke_joke}\n')
 
 #--------------------------------------------------------------------------
 # Part 2
@@ -29,28 +30,56 @@ dadjoke_search_data = response2.json()
 
 while True:
     #get subject search term for dad jokes from user
-    search_term = input("Enter the subject of dad joke or 'exit' to exit: ")
+    search_term = input("Enter the subject of dad joke or 'random' for a random joke or 'exit' to exit: ")
     search_term_url = 'http://icanhazdadjoke.com/search?term='+ search_term
+    # print(dadjoke_search_data)
+    
     if search_term == 'exit':
         break
+    if search_term == 'random':
+        response = requests.get('http://icanhazdadjoke.com/', headers=headers)
+        dadjoke_data = response.json()
+        dadjoke_joke = dadjoke_data['joke']
+        print(f'\nHere is a random Dad joke: ')
+        print(f'{dadjoke_joke}\n')
     else:
-        print(f'Here are the first 20 dad jokes mentioning "{search_term}":\n')
         response_search = requests.get(search_term_url, headers=headers)
         response_search_data = response_search.json()
         dadjoke_search_jokes = response_search_data['results']
-
         result_length = len(response_search_data['results'])
+        print(f'Here are {result_length} dad jokes mentioning "{search_term}":\n')
+                
         i = 0 
+        k = 1
         while i < result_length:
-            time.sleep(0.5)
+            time.sleep(0.35)
             print(f"{i+1} - {response_search_data['results'][i]['joke']}")
             i += 1
-
-        while True:
-            more_jokes = input(f'Would you like the next 20 jokes about {search_term} [y]es or [n]o: ').lower()
+            
+        while result_length == 20:
+            more_jokes = input(f'\nWould you like the next available Dad jokes about {search_term} [y]es or [n]o: ').lower()
             if more_jokes == 'n':
                 break
             elif more_jokes =='y':
-                next_jokes = 
+                k += 1
+                l = str(k)
+                next_jokes_url = 'https://icanhazdadjoke.com/search?term=' + search_term + '&page=' + l
+                next_jokes_raw = requests.get(next_jokes_url, headers=headers)
+                next_jokes_data = next_jokes_raw.json()
+                next_jokes = next_jokes_data['results']
+                next_jokes_length = len(next_jokes_data['results'])
+                j = 0
+                print(f"Here are the next {next_jokes_length} jokes: ")
+
+                while j < next_jokes_length:
+                    time.sleep(0.35)
+                    print(f"{j+1} - {next_jokes_data['results'][j]['joke']}")
+                    j += 1
+                    if j == next_jokes_length:
+                        break
+                if j < 20:
+                    break
+                print(result_length)
+
 
 
