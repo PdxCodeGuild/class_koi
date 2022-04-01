929d695ccbfcc37db46ce35535cac4a3a10f2ebf
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-04-01 15:51:25 -0700

    To be continued...

--- code/moss/python/lab16.py
+++ code/moss/python/lab16.py
@@ -1,12 +1,31 @@
 import requests
 from pprint import pprint
 
-response = requests.get('https://icanhazdadjoke.com/', headers = {'accept':'application/json'})
+#----- Version 1 -----#
 
-print(response)
-print('-'*72)
-print(response.text)
-print('-'*72)
-dad_joke = response.json()
-print(dad_joke['joke'])
-print('-'*72)
+# response = requests.get('https://icanhazdadjoke.com/', headers = {'accept':'application/json'})
+
+# print(response)
+# print('-'*72)
+# print(response.text)
+# print('-'*72)
+# dad_joke = response.json()
+# print(dad_joke['joke'])
+# print('-'*72)
+
+#----- Version 2 -----#
+
+print('\nWelcome to Dads Have Jokes...\n')
+
+while True:
+
+    search_term = input("\nWhat term would you like to search by? Enter a word or 'Q' to exit:\n").upper()
+
+    if search_term == 'Q':
+        print("\nGo ask Mom for jokes. Good-bye.\n")
+        break
+    
+    else:
+        response = requests.get('https://icanhazdadjoke.com/search', params = {'term':search_term, 'limit':'1'}, headers = {'accept':'application/json'})
+        dad_joke = response.json()                    #or-->search?term=search_term'
+        print(dad_joke)
