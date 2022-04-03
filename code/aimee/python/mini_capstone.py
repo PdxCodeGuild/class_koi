9d129c217be16c0b8074823cafb6cc1d5cb1b82e
Aimee Young <aimeeyoung@Aimees-MacBook-Pro-2.local>
2022-04-02 22:41:31 -0700

    progress

--- code/aimee/python/mini_capstone.py
+++ code/aimee/python/mini_capstone.py
@@ -5,3 +5,13 @@ Choose Your Own Adventure
 by Aimee Young
 '''
 
+print('''Welcome to <Name>, a choose-your-fate story adventure. 
+When prompted, make your choice by entering in the corresponding number. 
+Make your decisions, and see if you live or die...''')
+
+risk_counter = 0 
+
+
+
+
+
