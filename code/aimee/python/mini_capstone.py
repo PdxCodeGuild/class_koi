f77eaec0200180bae5d9e4e3e5d15919d5aa50c4
Aimee <aimeenyy@gmail.com>
2022-04-03 19:04:35 -0700

    progress

--- code/aimee/python/mini_capstone.py
+++ code/aimee/python/mini_capstone.py
@@ -4,10 +4,22 @@ Python Mini Capstone
 Choose Your Own Adventure
 by Aimee Young
 '''
+import time
+from colorama import init
+init()
 
-print('''Welcome to <Name>, a choose-your-fate story adventure. 
+# welcome
+print(f'''Welcome to <Name>, a choose-your-fate story adventure. 
 When prompted, make your choice by entering in the corresponding number. 
-Make your decisions, and see if you live or die...''')
+Make your decisions, and see if you live \n\nor die...\n''')
+time.sleep(2)
+
+# brief introduction
+print(back.MAGENTA + '-'*80 + '\n'  + 'HOW IT BEGINS')
+print('It\'s a cold Friday night in February, and you and your friends have rented'\
+    'an Airbnb for the weekend. The house is a beautiful, classic A-frame situated' \
+        ' on a remote road outside of Snoqualmie, Washington. There\'s fresh snow' \
+            ' on the ground, and ')
 
 risk_counter = 0 
 
