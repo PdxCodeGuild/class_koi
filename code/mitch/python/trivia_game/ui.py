5463be2d43db296b8738971f191b23992e00ccd8
chapman311 <fixerchapman@gmail.com>
2022-04-05 16:25:22 -0700

    progress save

--- code/mitch/python/trivia_game/ui.py
+++ code/mitch/python/trivia_game/ui.py
@@ -3,6 +3,12 @@ from appJar import gui
 def press(rb):
     print(app.getRadioButton("selection"))
 
+###################THIS needs work...
+def set_buttons():
+    app.setRadioButton("selection", "1AA", callFunction=False)
+    app.setRadioButton("selection", "2SS", callFunction=False)
+    app.setRadioButton("selection", "3GG", callFunction=False)
+    app.setRadioButton("selection", "4TT", callFunction=False)
 
 app = gui()
 app.addRadioButton("selection", "11")
@@ -12,6 +18,7 @@ app.addRadioButton("selection", "44")
 
 
 app.addButton("SUBMIT", press)
+app.addButton("SET BUTTONS", set_buttons)
 
 app.go()
 
