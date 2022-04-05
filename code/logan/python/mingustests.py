62140a8b13e798b1e79db9f43b9f33d7a30d8e96
logandouglass <logandouglass@gmail.com>
2022-04-05 14:23:56 -0400

    goofing around with mingus

---
+++ code/logan/python/mingustests.py
@@ -0,0 +1,16 @@
+# import mingus.core.notes as notes
+# print(notes.is_valid_note("C"))
+
+import mingus.core.chords as chords
+
+print(chords.determine("E", "G#", "D", "G"))
+
+# import mingus
+# print(mingus.core.notes.is_valid("C"))
+
+
+
+
+
+## Qs...
+# notes.to_minor("A")  Why doesn't this work?
