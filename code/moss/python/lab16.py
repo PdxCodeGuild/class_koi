3616fd01eac81f04178d69de0a53830864bbcda4
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-04-05 12:16:07 -0700

    Lab16 : Dad Joke API

--- code/moss/python/lab16.py
+++ code/moss/python/lab16.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from pprint import pprint
 
 #----- Version 1 -----#
@@ -18,14 +19,29 @@ from pprint import pprint
 print('\nWelcome to Dads Have Jokes...\n')
 
 while True:
-
+    
+    print('-'*72)
     search_term = input("\nWhat term would you like to search by? Enter a word or 'Q' to exit:\n").upper()
-
+    # more_jokes = int(input("\nHow many jokes would you like? Enter a number with maximum of 20.\n"))
+    
     if search_term == 'Q':
         print("\nGo ask Mom for jokes. Good-bye.\n")
         break
-    
+
     else:
-        response = requests.get('https://icanhazdadjoke.com/search', params = {'term':search_term, 'limit':'1'}, headers = {'accept':'application/json'})
-        dad_joke = response.json()                    #or-->search?term=search_term'
-        print(dad_joke)
+        response = requests.get('https://icanhazdadjoke.com/search', params = {'term':search_term, 'limit': 5 }, headers = {'accept':'application/json'})
+                                                                              #'page':num_page,'limit':more_jokes
+        # print(response)
+        # print('-'*72)
+        # print(response.text)
+        # print('-'*72)
+        jokes_data = response.json()['results']
+        
+        for j in jokes_data:
+            joke = j['joke']
+            time.sleep(1)
+            print('-'*72)
+            print(f'\n{joke}\n')
+            
+            
+    
