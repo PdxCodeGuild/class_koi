27e43f0badc8f93bb72988d22096091bc572fc2c
AdamFraley <adamfraley@gmail.com>
2022-04-07 09:57:17 -0700

    saving work on minicapstone

--- code/adam/python/minicap.py
+++ code/adam/python/minicap.py
@@ -25,15 +25,15 @@ import pandas as pd
 import pandas_datareader as pdr
 import requests
 import datetime
-# from IPython.display import display
-# import plotly.express as px
 import matplotlib.pyplot as plt
 
-start_year = 2020
-start_month = 5
+start_year = int(input('Enter the start year: '))
+start_month = int(input('Enter the start month (1-12): '))
 
 end_year = 2022
-end_month = 6
+end_month = 3
+
+series = 'PAYEMS' #PAYEMS WPSFD4131
 
 start = datetime.date (start_year, start_month, 1)
 end = datetime.date (end_year, end_month, 1)
@@ -41,27 +41,9 @@ series = 'PAYEMS' #PAYEMS WPSFD4131
 df = pdr.DataReader(series, 'fred', start, end)
 
 print(df)
-# display(df)
 print(type(df))
 df.plot()
 plt.show()
 
-# start_date = "2020-01-1"
-# end_date = "2020-12-31"
-# data = web.DataReader(name="TSLA", data_source='yahoo', start=start_date, end=end_date)
-# print(data)
 
 
-
-# fig = px.line(df, y='PAYEMS',
-#               title='Total Nonfarm Payrolls 2005-2020',
-#               labels={'PAYEMS':'All Nonfarm Employees'})
-
-# fig.show()
-
-# category_id = '125'
-# response = requests.get('https://api.stlouisfed.org/fred/category?category_id='+category_id+'&api_key=' + fred_api_key + '&file_type=json')
-# fred_data = response.json()
-# # print(fred_data)
-
-# display(df)
---
+++ code/adam/python/test2.py
@@ -0,0 +1,28 @@
+import easygui
+
+from random_quote import random_quote
+
+test_quote = {
+	'quote': "At least once a year I like to bring in some of my Kevin's Famous Chili. The trick is to undercook the onions. Everybody is going to get to know eachother in the pot. I'm serious about this stuff. I'm up the night before, pressing garlic, and dicing whole tomatoes. I toast my own ancho chiles. It's a recipe passed down from Malones for generations - it's probably the thing I do best.",
+	'character': 'Kevin Malone',
+}
+
+while True:
+    # quote = test_quote
+    quote = random_quote()
+
+    message = f"""
+{quote['quote']}
+––{quote['character']}
+"""
+
+    another_quote = easygui.boolbox(message, 'The Office Quote', ['Another Quote', 'Done'])
+    if not another_quote:
+        break
+# user_input = easygui.textbox('type something ')
+
+# easygui.msgbox(f'you typed: {user_input}')
+
+# user_input = input('type something ')
+
+# print('you typed ' + user_input)
