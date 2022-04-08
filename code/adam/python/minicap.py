f13d436ac93018438fce0f1244fe3623a95610c3
AdamFraley <adamfraley@gmail.com>
2022-04-07 17:06:06 -0700

    working on form validation

--- code/adam/python/minicap.py
+++ code/adam/python/minicap.py
@@ -20,22 +20,45 @@ def stock_get(start, end):
     df_stk = web.DataReader(stock_ticker, 'yahoo', start, end)['Adj Close']
     return df_stk
 
-# plot 2 graphs FRED api data on top graph and stock prices on bottom with shared time axis
+# plot 2 graphs of data sets with shared time axis
 def plot_graphs(df1, df_stk):
     fig, axes = plt.subplots(2,1,sharex=True)
     df1.plot(ax=axes[0])
     df_stk.plot(ax=axes[1])
     plt.show()
 
+# plot single set of data across specified time
+def plot_graph1(df):
+    df.plot()
+    plt.show()
+
+# get start year and month of data from user
 def get_time_start():
     start_year = int(input('Enter the start year: '))
+    while True:
+        if start_year < 1920 or start_year > 2022:
+            start_year =int(input('Please enter a start year between 1920-2022: '))
+        else:
+            break
     start_month = int(input('Enter the start month (1-12): '))
     start = datetime.date (start_year, start_month, 1)
     return start
+# get end year and month of data from user
 def get_time_end():
-    end_year = int(input('Enter the ending year: '))    
+    end_year = int(input('Enter the ending year: '))  
     end_month = int(input('Enter the end month (1-12): '))
     end = datetime.date (end_year, end_month, 1)
+    while True:
+        if end.year == start.year and end.month < start.month:
+            print('End date must be after Start date')
+            end_year = int(input(f'Please enter an end year after the start date "{start}": '))
+            end_month = int(input(f'Please enter a month after the start date "{start}": '))
+        if end.year < start.year:
+            print('End date must be after Start date')
+            end_year = int(input(f'Please enter an end year after the start date "{start}": '))
+            end_month = int(input(f'Please enter a month after the start date "{start}": '))
+        else:
+            break
     return end
 
 # df_stk = stock_get(start, end)
@@ -87,19 +110,15 @@ while True:
         start = get_time_start()
         end = get_time_end()
         df_stk = stock_get(start, end)
-        df_stk.plot()
-        plt.show()
+        plot_graph1(df_stk)
     if q1 == '5':
         print('\nGraph of Economic indicator over specified time:')
         start = get_time_start()
         end = get_time_end()
         df1 = fred_get(start, end)
-        df1.plot()
-        plt.show()
+        plot_graph1(df1)
 
         
-
-
 
 ## Collects 2 series using datareader and plots both series on same graph
 # series1 = 'CPIAUCSL'
