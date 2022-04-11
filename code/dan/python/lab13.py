8e73c1267921280d98ec1d6bed6b9ed16d898176
asddataking <asddataking@outlook.com>
2022-04-11 14:24:55 -0400

    This should be the right version

---
+++ code/dan/python/lab13.py
@@ -0,0 +1,69 @@
+class ATM:
+    def __init__(self, balance=0, interest_rate=0.1):
+        self.balance = 0
+        self.interest_rate = 0.1
+        
+
+    def check_balance(self):
+       return self.balance 
+
+    def deposit(self, amount):
+        self.balance += amount
+        """deposit a given amount into account"""
+        ...
+
+    def check_withdrawal(self, amount):
+        if amount < self.balance:
+            return True
+        """return True if account has enough funds to withdraw given amount"""
+        ...
+
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+        return self.balance 
+        """withdraw given amount from account and return that amount"""
+        ...
+
+    def calc_interest(self):
+        interest_accured = self.balance * self.interest_rate
+        return interest_accured 
+        
+        """calculate and return interest gained on account"""
+
+
+
+
+atm = ATM()  # create an instance of our class
+print('Welcome to the ATM')
+while True:
+    command = input('Enter a command: ')
+    if command == 'balance':
+        balance = atm.check_balance()  # call the check_balance() method
+        print(f'Your balance is ${balance}')
+    elif command == 'deposit':
+        amount = float(input('How much would you like to deposit? '))
+        atm.deposit(amount)  # call the deposit(amount) method
+        print(f'Deposited ${amount}')
+    elif command == 'withdraw':
+        amount = float(input('How much would you like '))
+        # call the check_withdrawal(amount) method
+        if atm.check_withdrawal(amount):
+            atm.withdraw(amount)  # call the withdraw(amount) method
+            print(f'Withdrew ${amount}')
+        else:
+            print('Insufficient funds')
+    elif command == 'interest':
+        amount = atm.calc_interest()  # call the calc_interest() method
+        atm.deposit(amount)
+        print(f'Accumulated ${amount} in interest')
+    elif command == 'help':
+        print('Available commands:')
+        print('balance  - get the current balance')
+        print('deposit  - deposit money')
+        print('withdraw - withdraw money')
+        print('interest - accumulate interest')
+        print('exit     - exit the program')
+    elif command == 'exit':
+        break
+    else:
+        print('Command not recognized')
