b6380e44c4231b355558647b51c82cacf5cff73a
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-04-13 11:51:50 -0700

    file update

--- code/moss/html-css/flask_lab01/app.py
+++ code/moss/html-css/flask_lab01/app.py
@@ -3,35 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
 
-@app.route('/laugh')
-def laugh():
-    return 'HAHAHAHAHA'
-
-@app.route('/random_msg')
-def random_msg():
-    from random import choice
-    messages = [
-        'Flask is  fun',
-        'CSS is fun',
-        'HTML is fun',
-    ]
-    return choice(messages)
-
-@app.route('/template')
-def template():
-    from random import choice
-    messages = [
-        'Flask is  fun',
-        'CSS is fun',
-        'HTML is fun',
-    ]
-    view_message = choice(messages)
-    programmer = "Moss"
-    framework = "Flask"
-    return render_template('index.html', 
-    template_message = view_message, programmer = programmer,
-    framework = framework)
 
 app.run(debug=True)
