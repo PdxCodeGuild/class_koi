f7a9157d4a2c3dec810697c1d2701ad89a4ede06
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-04-14 11:49:49 -0700

    Lab01 Bio, Flasked

--- code/moss/html-css/flask_lab01/app.py
+++ code/moss/html-css/flask_lab01/app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    with open('jmkovic.json') as f:
+        data = json.load(f)
+        print(data)
+        print(type(data)
+    )
 
+    return render_template(
+        'index.html',
+        name = data.get('name'),
+        bio = data.get('bio'),
+        link1 = data.get('link1'),
+        link2 = data.get('link2'),
+        img = data.get('img'),
+        places_lived = data.get('places_lived'),
+        quote = data.get('quote'),
+    )
 
 app.run(debug=True)
