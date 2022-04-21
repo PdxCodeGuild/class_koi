27d82b7f716036f52e0f0cfe5ebf9449c0e1b386
asddataking <asddataking@outlook.com>
2022-04-21 18:20:00 -0400

    Lab 6 currently working

--- code/dan/python/lab06.py
+++ code/dan/python/lab06.py
@@ -2,20 +2,22 @@
 
 
 # Dictionary "Number of matches", "Jackpot Amount"
-winnings_dict= {1:"$4",
-                2: "$7",
-                3 : "$100",
-                4: "$50,000",
-                5 : "$1,000,000",
-                6 : "25,000,000"}
-
+winnings_dict= {
+                0:0,
+                1:4,
+                2: 7,
+                3 : 100,
+                4: 50_000,
+                5 : 1_000_000,
+                6 : 25_000_000,
+}
 import random
 
 #empty lists to append
 ticket = []
 winning_ticket = []
 balance = 0
-balance = -2
+expense = 0
 
 # takes in a list adds 6 random numbers to list
 def pick6(list):
@@ -32,37 +34,53 @@ print(pick6(ticket))
 print(pick6(winning_ticket))
 
 # function to check numbers in ticket & winning ticekt match in correct index
-matches = 0
+
 def num_matches(ticket, winning_ticket):
-   global matches # global used to return matches outside of function 
-for x in range (len(ticket)):
-        if ticket[x] == winning_ticket[x]:
-            matches += 1
-        print (matches)
+    matches = 0
+    #global matches # global used to return matches outside of function 
+    for x in range (len(ticket)):
+            if ticket[x] == winning_ticket[x]:
+                matches += 1
+            return matches
+
+print (num_matches(ticket, winning_ticket))
+    
          
 
 # print winner logic
-if matches == 0:
-   print("Sorry try again next time")
-elif matches == 1:
-   print (f"You've won {winnings_dict[1]} + {balance}")
-elif matches == 2:
-    print (f"You've won {winnings_dict[2]} + {balance}")
-elif matches == 3:
-    print (f"You've won {winnings_dict[3]} + {balance}")
-elif matches == 4:
-    print (f"You've won {winnings_dict[4]} + {balance}")
-elif matches == 5:
-    print (f"You've won {winnings_dict[5]} + {balance}")
-elif matches == 6:
-    print (f"You've won the grand prize {winnings_dict[6]} + {balance}")
+#if matches == 0:
+   #print("Sorry try again next time")
+#elif matches == 1:
+  # print (f"You've won {winnings_dict[1]} + {balance}")
+#elif matches == 2:
+   # print (f"You've won {winnings_dict[2]} + {balance}")
+#elif matches == 3:
+    #print (f"You've won {winnings_dict[3]} + {balance}")
+#elif matches == 4:
+    #print (f"You've won {winnings_dict[4]} + {balance}")
+#elif matches == 5:
+    #print (f"You've won {winnings_dict[5]} + {balance}")
+#elif matches == 6:
+    #print (f"You've won the grand prize {winnings_dict[6]} + {balance}")
+    
+earnings = 0
+
+for i in range (100_000):
+    empty_ticket= []
+    
+    ticket = pick6(empty_ticket)
+    matches = num_matches(ticket, winning_ticket) 
+    balance -= 2
+    expense += 2
+    balance += winnings_dict[matches]
+    earnings += winnings_dict[matches]
     
     
+print(matches)   
+print (balance)
+print ((earnings - expense)/expense)
+    
 
-for i in range (100000):
-    pick6(ticket)
-    num_matches(winning_ticket, ticket) 
 
 
-ROI = balance + winning_ticket[]
     
