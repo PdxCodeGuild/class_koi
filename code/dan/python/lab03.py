b30b677fa898139714369e63ee46f2dbdc7b46b1
asddataking <asddataking@outlook.com>
2022-04-25 18:10:54 -0400

    Lab 3 cleaned up

--- code/dan/python/lab03.py
+++ code/dan/python/lab03.py
@@ -1,28 +1,23 @@
-#coins = [
-  #  ('half-dollar', 50),
-  #  ('quarter', 25),
-   # ('dime', 10),
-   # ('nickel', 5),
-   # ('penny', 1)
-#]
+coins = [
+   ('half-dollar', 50),
+    ('quarter', 25),
+   ('dime', 10),
+   ('nickel', 5),
+   ('penny', 1),
+]
 
 #doesthiswork = total / coins[0][1]
 #print (doesthiswork)
 
-#Version 1 of Lab 03 / not complete 
 
 amount = input("Please enter a dollar amount" )
-amount = float (amount)
+amount = float(amount)
 print (amount) 
 
 
-quarters = amount // .25
-new_total =  amount - (quarters * 25)
-print (new_total)
-dimes = new_total  / .10
-nickels = amount / .05
+
+quarters = amount // .25 
+dimes = amount // .10
+nickels = amount // .05
 pennies = amount  / .01
 print (f"This uses {quarters} quarters, {dimes} dimes , {nickels} nickels , {pennies} pennies") 
-
-
- 
