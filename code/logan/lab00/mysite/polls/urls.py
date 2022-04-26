54372ca7d58635b2d38afa1b1f97efe874e56835
logandouglass <logandouglass@gmail.com>
2022-04-26 15:18:07 -0400

    what the hell am i doing??  beginning part 4 soon...

--- code/logan/lab00/mysite/polls/urls.py
+++ code/logan/lab00/mysite/polls/urls.py
@@ -1,9 +1,20 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.contrib import admin # not used?
+from django.urls import include, path # not using include?
 from . import views
 
+
+app_name = 'polls'
+
 urlpatterns = [
+    # path('', views.index, name='index'),
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
 
