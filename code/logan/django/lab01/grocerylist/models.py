d8952e836b6dccfead5edf9d7293b77257eba1c0
logandouglass <logandouglass@gmail.com>
2022-04-27 12:38:52 -0400

    grahaha the hunt is on, kois

--- code/logan/django/lab01/grocerylist/models.py
+++ code/logan/django/lab01/grocerylist/models.py
@@ -5,3 +5,4 @@ class Grocery(models.Model):
     description = models.CharField(max_length=200)
     created_date = models.DateField()
     last_name = models.CharField(max_length=200)
+    completed = models.BooleanField()
