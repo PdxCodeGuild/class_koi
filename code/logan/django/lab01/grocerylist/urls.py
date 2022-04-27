7a6ca06859fd9f174145ce0eb2a1ad4da89bb41a
logandouglass <logandouglass@gmail.com>
2022-04-27 14:56:30 -0400

    coming together... basic index is a go

--- code/logan/django/lab01/grocerylist/urls.py
+++ code/logan/django/lab01/grocerylist/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 app_name = 'grocerylist'
 urlpatterns = [
-    path('index/', views.index, name='index')
+    path('', views.index, name='index'),
+    path("delete/", views.delete, name="delete"),
+    path("complete/", views.complete, name="complete")
+
 ]
+
+
+
+# index/
--- code/logan/django/lab01/grocerylist/views.py
+++ code/logan/django/lab01/grocerylist/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Grocery
 
 # Create views below
 def index(request):
-    return HttpResponse("you're home, baby")
+    latest_groceries = Grocery.objects.all()
+    context = {'latest_groceries': latest_groceries}
+    return render(request, "grocerylist/index.html", context)
+
+def complete(request):
+    return HttpResponse("you're completing, baby")
+
+def delete(request):
+    return HttpResponse("you're deleting, baby")
