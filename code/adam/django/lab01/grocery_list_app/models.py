09cab1d2c1ab2667307e23d14c12ffbb3671d86d
AdamFraley <adamfraley@gmail.com>
2022-04-27 10:55:20 -0700

    another progress save

--- code/adam/django/lab01/grocery_list_app/models.py
+++ code/adam/django/lab01/grocery_list_app/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 
 class GroceryItem(models.Model):
-    groceryitem_text = models.CharField(max_length=100)
-    pub_date = models.DateTimeField('date published')
+    description = models.CharField(max_length=100)
+    created_date = models.DateTimeField('date created')
+    completed_date = models.DateTimeField('date completed')
+    completed = models.BooleanField('completed')
