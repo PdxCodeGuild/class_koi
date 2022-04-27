42815edb4d1a2577b9e0492c21a6dd5f0ee32645
logandouglass <logandouglass@gmail.com>
2022-04-27 17:09:28 -0400

    django-ing intensifies...about to test new index

--- code/logan/django/lab01/grocerylist/views.py
+++ code/logan/django/lab01/grocerylist/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 from .models import Grocery
 
 # Create views below
 def index(request):
+    if request.method == 'POST': # this code will only run if it gets a post request
+        print(request.POST)
+        name = request.POST.get('name')
+        created_date = request.POST.get('created date')
+        completed_date = request.POST.get('completed date')
+        completed = request.POST.get('completed')
+        # Model.object.create(**kwargs) will create a new instance
+        # of that model and save it to the database
+
+        groc = Grocery.objects.create(
+            name=name,
+            created_date = created_date,
+            completed_date = completed_date,
+            completed = completed,
+            )
+        return redirect('/cities')
+
+
     latest_groceries = Grocery.objects.all()
-    context = {'latest_groceries': latest_groceries}
+    completed_list = Grocery.objects.filter(completed=True)
+    incompleted_list = Grocery.objects.filter(completed=False) 
+    context = {
+        'latest_groceries': latest_groceries,
+        'completed list' : completed_list,
+        'incomplete_list' : incompleted_list,
+    }
+    
     return render(request, "grocerylist/index.html", context)
 
 def complete(request):
