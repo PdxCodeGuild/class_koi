936e1b127ae6e5cf31461aeb20141ea2aee6d789
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-04-27 12:27:20 -0700

    Tutorial 4

--- code/moss/django/lab00/polls/polls/polls_app/models.py
+++ code/moss/django/lab00/polls/polls/polls_app/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.db import models
-from django import timezone
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
