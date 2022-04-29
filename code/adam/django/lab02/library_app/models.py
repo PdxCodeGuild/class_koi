36d6d8fd4674edb53ed000c1a0eddd3b24a240aa
AdamFraley <adamfraley@gmail.com>
2022-04-29 10:40:55 -0700

    progress save

--- code/adam/django/lab02/library_app/models.py
+++ code/adam/django/lab02/library_app/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.utils import timezone
 
-class Book(models.Model):
-    title = models.CharField(max_length=50)
-    publish_date = models.DateField('date published', default=)
+class Author(models.Model):
+    name = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
 
-class Author(models.Model):
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    publish_date = models.DateField('date published', null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.name
 
--- code/adam/django/lab02/library_app/views.py
+++ code/adam/django/lab02/library_app/views.py
@@ -1,6 +1,16 @@
+from xml.dom.pulldom import CHARACTERS
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 
+from .models import Author, Book
+
 def index(request: HttpRequest):
-    
+    authors = Author.objects.all()
+    books = Book.objects.all()
+    context = {
+        'authors': authors,
+        'characters': books,
+    }
+    return HttpResponse("hell, world, you are at the library") #request, '/library')
+
 
