dcf5239a28c1b12e8f77a2782d02a46f9d33ee94
AdamFraley <adamfraley@gmail.com>
2022-04-29 10:51:16 -0700

    save

--- code/adam/django/lab02/library_proj/urls.py
+++ code/adam/django/lab02/library_proj/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import inlcude, path
+from django.urls import include, path
 
 urlpatterns = [
-    path('')
+    path('library_app/', include('library_app.urls')),
     path('admin/', admin.site.urls),
 ]
