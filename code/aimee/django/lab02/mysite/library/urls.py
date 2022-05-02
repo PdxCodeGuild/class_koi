f539155185ace510b617ea479db3cf0f81de975d
Aimee <aimeenyy@gmail.com>
2022-05-02 16:35:59 -0700

    completed library lab 02

--- code/aimee/django/lab02/mysite/library/urls.py
+++ code/aimee/django/lab02/mysite/library/urls.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 
 from . import views
 
+app_name = 'library'
 urlpatterns = [
     path('', views.index),
     path('details/<int:id>/', views.details),
-    path('checkouts/<int:id>/', views.checkouts)
+    path('checkouts/<int:id>/', views.checkouts),
+    path('records/', views.records)
 ]
+
+# The current path, library/details/1/checkouts/1, didn’t match any of these. 
--- code/aimee/django/lab02/mysite/library/views.py
+++ code/aimee/django/lab02/mysite/library/views.py
@@ -39,12 +39,23 @@ def details(request, id):
         }
     return render(request, 'library/details.html', context)
 
+def records(request):
+    book = Book.objects.all()
+    record = CheckOut.objects.all()
+    time = request.POST.get('timestamp')
+    context = { 'record':record, 'book':book, 'time':time, }
+    return render(request, 'library/records.html', context)
+
 # not finished
 def checkouts(request, id):
-    checked_out = CheckOut.objects.get(id=id)
-    # if ID is 1, book is checked out
-    checked_out.checkout = True
-    checked_out.save()
-    context = {'checked_out':checked_out,}
-    return redirect('/library/')
+    book = Book.objects.get(id=id)
+    if request.method == 'POST':
+        status_update = request.POST.get('status_update')
+        username = request.POST.get('username')
+        CheckOut.objects.create(
+            book=book, user=username, checkout=status_update,
+        )
+    return redirect(f'/library/details/{id}', id=id)
 
+# def returns(request, id):
+#     ...redirect('/details/', id=id)
