aad2cb8b9f72d2e52b8e8aadf4166d69d29f6b31
pjz987 <pwj2012@gmail.com>
2022-05-02 13:42:27 -0700

    fixing lab03 with dan

--- code/dan/python/lab03.py
+++ code/dan/python/lab03.py
@@ -11,13 +11,16 @@ coins = [
 
 
 amount = input("Please enter a dollar amount" )
-amount = float(amount)
+amount = float(amount) * 100
 print (amount) 
 
 
 
-quarters = amount // .25 
-dimes = amount // .10
-nickels = amount // .05
-pennies = amount  / .01
+quarters = amount // 25 
+amount %= 25
+dimes = amount // 10
+amount %= 10
+nickels = amount // 5
+amount %= 5
+pennies = amount  / 1
 print (f"This uses {quarters} quarters, {dimes} dimes , {nickels} nickels , {pennies} pennies") 
