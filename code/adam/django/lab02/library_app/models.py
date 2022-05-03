32f5fa58cdf91ce910f6f9f27a4cb59817ad208e
AdamFraley <adamfraley@gmail.com>
2022-05-03 16:28:37 -0700

    library-added genres, still working on user authentication

--- code/adam/django/lab02/library_app/models.py
+++ code/adam/django/lab02/library_app/models.py
@@ -12,6 +12,10 @@ class Book(models.Model):
     publish_date = models.DateField('date published', null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.PROTECT)
     checked_out = models.BooleanField('checked-out', default=False)
+    genres = models.ManyToManyField('Genre', related_name='books')
+
+    def comma_separated_genres(self):
+        return ', '.join([str(genre) for genre in self.genres.all()])
 
     def __str__(self):
         return f'{self.title} by {self.author}'
@@ -21,6 +25,16 @@ class Checkout(models.Model):
     book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name='checkouts')    
     checked_out_date = models.DateField('date checked-out', null=True, blank=True)
     checked_in_date = models.DateField('date checked in', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.checked_out_by} - {self.book.title}'
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__ (self):
+        return self.name
+    
     
 
 
