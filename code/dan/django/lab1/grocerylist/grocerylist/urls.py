986bbd6210efda650eb5f8e26dea2131519c24b6
asddataking <asddataking@outlook.com>
2022-05-04 18:12:18 -0400

    Django Lab 1

--- code/dan/django/lab1/grocerylist/grocerylist/urls.py
+++ code/dan/django/lab1/grocerylist/grocerylist/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from operator import index
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', include ('myapp.urls')),
+    
 ]
--- code/dan/django/lab1/grocerylist/myapp/models.py
+++ code/dan/django/lab1/grocerylist/myapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class GroceryItem(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200)
-    completed_date = models.CharField(max_length=200)
-    iteam_boolean = models.BooleanField(max_length=200)
+    created_date = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(null=True,default=False)
     
     def __str__(self):
-        return self.myfield
+        return self.item_name
