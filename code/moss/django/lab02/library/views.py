bd9332d9940d3b1ff6bf7ea8b2d329e07ca49339
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-05-04 14:45:53 -0700

    Set-up

--- code/moss/django/lab02/library/views.py
+++ code/moss/django/lab02/library/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Author, Book
+from .models import Book
 
 
 def index(request):
     inventory = Book.objects.all()
-    return render(request,'library/index.html',{inventory:inventory})
+    return render(request,'library/index.html',{'inventory':inventory})
 
 
     
