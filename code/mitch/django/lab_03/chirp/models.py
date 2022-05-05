44a98fc363739434e412d249e72c5bdb7633d3ad
chapman311 <fixerchapman@gmail.com>
2022-05-05 16:32:34 -0700

    Lab 03 progress

--- code/mitch/django/lab_03/chirp/models.py
+++ code/mitch/django/lab_03/chirp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ChirpPost(models.Model):
+    text = models.CharField(max_length=128)
+    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='grocery_items')
+    
+    def __str__(self):
+        return f'{self.user}@ {self.date}: {self.text}'
--- code/mitch/django/lab_03/chirp/views.py
+++ code/mitch/django/lab_03/chirp/views.py
@@ -1,7 +1,20 @@
-from django.http import HttpRequest
-from django.shortcuts import render
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from .models import ChirpPost
 
 def index(request):
-    return render(request, 'chirp/index.html')
-
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return redirect('users:login')
+        text = request.POST.get('post_text')
+        ChirpPost.objects.create(
+            text=text,
+            user=request.user
+        )
+        return redirect('chirp:index')
+    context = {}
+    return render(request, 'chirp/index.html', context)
+    
+    
