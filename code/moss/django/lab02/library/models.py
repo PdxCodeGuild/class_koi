5d7f5eec2cd90c5f42604fa16346655a88f20c66
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-05-05 12:24:10 -0700

    Set-up

--- code/moss/django/lab02/library/models.py
+++ code/moss/django/lab02/library/models.py
@@ -3,12 +3,13 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=30)
+   
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author,on_delete=models.CASCADE)
     pub_date = models.DateField()
 
--- code/moss/django/lab02/library/views.py
+++ code/moss/django/lab02/library/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Author, Book
 
 
 def index(request):
     inventory = Book.objects.all()
-    return render(request,'library/index.html',{'inventory':inventory})
+    author = Author.objects.all()
+    return render(request,'library/index.html',{'inventory':inventory, 'author':author})
 
 
     
