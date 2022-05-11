2517823b2bf3cfb6cda06cbdce4c12e194115ea7
asddataking <asddataking@outlook.com>
2022-05-11 19:22:01 -0400

    Pick 3 1 done

--- code/dan/javascript/Pick3/makeChange/part_a/script.js
+++ code/dan/javascript/Pick3/makeChange/part_a/script.js
@@ -1,16 +1,26 @@
 let amount = prompt("Please enter a dollar amount")
 
 
+amount = parseFloat(amount) * 100
+console.log(amount)
 
-let quarters = amount / 25 
-amount % 25
+
+let quarters = (amount) / 25 
+console.log(parseInt(quarters))
+amount = parseFloat(amount) % 25
 console.log(amount)
+
 let dimes = amount / 10
-amount %= 10
+if (parseFloat(amount) > 10){
+    amount= parseFloat(amount) % 10
+}
 console.log(amount)
+
 let nickels = amount / 5
-amount += amount % 5
+amount = parseFloat(amount) % 5
 console.log(amount)
 let pennies = amount  / 1
+amount = parseFloat(amount) % 1
 console.log(amount)
-console.log (`'This uses' ${quarters} 'quarters', ${dimes} 'dimes' , ${nickels} 'nickels' , ${pennies} 'pennies'`) 
+
+console.log (`'This uses +' ${parseInt(quarters)} 'quarters', ${parseInt(dimes)} 'dimes' , ${parseInt(nickels)} 'nickels' , ${pennies} 'pennies'`) 
