528dd4df45ab43b9733b00e7f2517d1a7a92a743
MattDWalsh <matthew.david.walsh@gmail.com>
2022-05-13 09:30:21 -0700

    began optional bouncing ball lab

---
+++ code/matt/javascript/optional/bouncingBall/script.js
@@ -0,0 +1,18 @@
+let cnv = document.querySelector('.ballworld')
+let ctx = cnv.getContext("2d")
+
+let width = 500
+let height = 500
+
+let ball = {
+    radius: 4,
+    px: Math.random()*width,
+    py: Math.random()*height,
+    vx: (2*Math.random()-1)*10,
+    vy: (2*Math.random()-1)*10
+};
+
+ctx.beginPath();
+ctx.arc(ball.px, ball.py, ball.radius, 0, 2 * Math.PI, false);
+ctx.fillStyle = 'green';
+ctx.fill();
