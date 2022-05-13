68dd7613e57fd2f861bec46af4aa9810560a8b7c
Nathan Bonilla <nbonilla409@gmail.com>
2022-05-12 18:39:34 -0700

    committing django/lab02

--- code/nathan/django/lab02/library/views.py
+++ code/nathan/django/lab02/library/views.py
@@ -7,20 +7,27 @@ from .models import Book, Author, User
 def index(request):
     books = Book.objects.all()
     available_books = Book.objects.filter(checked_out = False)
-    # check_in_books = User.objects.filter(checkout = True)
-    check_in_books = User.objects.all()
+    # show all users with the checkout bool as true and 
+        # show only the first of those users
+    check_in_books = User.objects.filter(checkout = True)
     hide_list = []
     show_list = []
-    for i in check_in_books:
+    for index, i in enumerate(check_in_books):
         if(i in hide_list or i in show_list):
             continue
-        elif(i.checkout == False):
+        elif(i.checkout == False and i in show_list):
+            show_list.pop(i.name)
+        elif(i.checkout == False and i not in hide_list):
             hide_list.append(i)
         elif(i.checkout == True and i not in hide_list and i not in show_list):
             show_list.append(i)
+        # if(index == len(check_in_books)):
+        #     hide_list = hide_list.clear()
     
-    print(show_list)
-    print(hide_list)
+    print(f'show list {show_list}')
+    print(f'hide list {hide_list}')
+    # hide_list = []
+    # show_list = []
 
     library_history = User.objects.all()
     print(type(library_history))
@@ -31,6 +38,7 @@ def index(request):
         'check_in_books': show_list,
         'library_history': library_history,
     }
+
     return render(request, 'index.html', context)
 
 def user_checkout(request):
@@ -47,7 +55,7 @@ def user_checkout(request):
 
         return redirect('/')
 
-def user_checkin(request):
+def user_checkin(request, show_list):
     if(request.method == 'POST'):
         name = request.POST.get('check_in_book')
         target_user:User = User.objects.filter(name = name).first()
@@ -57,4 +65,6 @@ def user_checkin(request):
         User.objects.create(name = target_user.name, book = target_user.book, checkout = False, timestamp = timezone.now())
         target_book.checked_out = False
         target_book.save()
+
+        show_list = []
         return redirect('/')
