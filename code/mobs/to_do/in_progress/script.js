21291af515ab8fc16a1b910dfa1735a8fa9b64d9
logandouglass <logandouglass@gmail.com>
2022-05-13 17:53:05 -0400

    optional completed

--- code/mobs/to_do/in_progress/script.js
+++ code/mobs/to_do/in_progress/script.js
@@ -1,4 +1,4 @@
-localStorage.clear()
+// localStorage.clear()
 
 let newItem = document.querySelector("#type_text")
 let addButton = document.querySelector("#add_button")
@@ -21,6 +21,10 @@ if(localStorage.getItem('completedArray')) {
     completedArray = []
 }
 
+toDoArray.forEach(buildItem)
+// Place later if fail happens
+completedArray.forEach(buildItem)
+
 function buildItem (newItem) {
     let toDoTask = document.createElement("li")
     let toDoName = document.createElement("p")
@@ -31,25 +35,40 @@ function buildItem (newItem) {
     toDoRemove.innerText = "Remove"
     toDoComplete.innerText = "Complete"
     toDoTask.appendChild(toDoRemove)
-    toDoTask.appendChild(toDoComplete)
-    todoList.appendChild(toDoTask)
+    if (toDoArray.includes(newItem)) {
+        toDoTask.appendChild(toDoComplete)
+        todoList.appendChild(toDoTask)
+    }
+    else {
+        completeList.appendChild(toDoTask)
+    }
 
     toDoRemove.addEventListener("click", function(_) {
+        toDoTask.remove()
+        if (toDoArray.includes(newItem)){
+            let index = toDoArray.indexOf(newItem)
+            toDoArray.splice(index,1)
+            localStorage.setItem('toDoArray', JSON.stringify(toDoArray))
+        }
+        else {
+            let index = completedArray.indexOf(newItem)
+            completedArray.splice(index,1)
+            localStorage.setItem('completedArray', JSON.stringify(completedArray))
+        }
 
-        toDoArray.remove(toDoTask)
-        localStorage.setItem('toDoArray', JSON.stringify(toDoArray))
     })
     toDoComplete.addEventListener("click", function(evt) {
        toDoTask.remove() 
        toDoComplete.remove()
        completeList.appendChild(toDoTask)
        let index = toDoArray.indexOf(newItem)
-    //    alert(newItem)
+
        let completed = toDoArray.splice(index,1)
        completedArray.push(completed)
-       alert(`todo:${toDoArray}completed:${completedArray}`)
+    //    alert(`todo:${toDoArray}completed:${completedArray}`)
        localStorage.setItem('toDoArray', JSON.stringify(toDoArray))
        localStorage.setItem('completedArray', JSON.stringify(completedArray))
+    
     })
 }
 
