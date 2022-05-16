278aa9f9849ac608e2ac24191a53b88a7cea2ce4
Brandon Sarazin <bsarazin1@gmail.com>
2022-05-16 18:24:54 -0400

    lab03

--- code/Brandon/django/lab03/chirp/posts/models.py
+++ code/Brandon/django/lab03/chirp/posts/models.py
@@ -1,15 +1,26 @@
+from asyncio.proactor_events import _ProactorSocketTransport
 from tabnanny import verbose
 from django.db import models
 
+from django.utils import timezone
 # Create your models here.
 
 
-class Posts(models.Model):
-    post = models.CharField(max_length=128)
-    posted_time = models.DateTimeField(null=True, blank=True)
+class Users(models.Model):
+    user_name = models.CharField(max_length=40)
 
     def __str__(self):
-        return self.post
+        return self.user_name
+
+
+class Posts(models.Model):
+    posted_message = models.CharField(max_length=128, null=True, blank=True)
+    posted_time = models.CharField(max_length=40, blank=True)
+    author = models.ForeignKey(
+        Users, on_delete=models.PROTECT, related_name='auth', blank=True, null=True)
+
+    def __str__(self):
+        return self.posted_message
 
     class Meta:
         verbose_name_plural = 'Posts'
--- code/Brandon/django/lab03/chirp/posts/views.py
+++ code/Brandon/django/lab03/chirp/posts/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Posts, Users
 
-import datetime
 
 from django.http import HttpResponse
-from .models import Posts
+from django.utils import timezone
 
 
 def index(request):
     posts = Posts.objects.all()
+    authors = Users.objects.all()
     if request.method == 'POST':
-        post = request.POST.get('post')
-        posted_time = datetime.datetime.now()
-    return render(request, 'posts/index.html')
+        print('hello')
+        posted_message = request.POST.get('posted_message')
+        posted_time = timezone.now().replace(microsecond=0)
+        user_name = request.user.username
+        author, created = Users.objects.get_or_create(
+            user_name=user_name)
+        Posts.objects.create(
+            posted_message=posted_message,
+            posted_time=posted_time,
+            author=author
+        )
+        return redirect('posts:index')
+    context = {
+        'posts': posts,
+        'authors': authors
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def user_page(request):
+
+    posts = Posts.objects.all()
+    authors = Users.objects.all()
+    context = {
+        'posts': posts,
+        'authors': authors
+    }
+
+    return render(request, '<int:id>/account.html', context)
