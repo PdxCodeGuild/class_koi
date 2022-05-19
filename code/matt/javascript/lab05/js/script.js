91d539065d1c3c2dfd7b56e89456555d9d6d2bb8
MattDWalsh <matthew.david.walsh@gmail.com>
2022-05-19 16:30:45 -0700

    added basic styling, cleaned up functionality

--- code/matt/javascript/lab05/js/script.js
+++ code/matt/javascript/lab05/js/script.js
@@ -1,17 +1,3 @@
-let page = 1
-let keyword = 'dog'
-
-// axios({
-// 	url: `https://favqs.com/api/quotes/?page=${page}&filter=${keyword}`,
-// 	method: 'get',
-// 	headers: {
-// 		Accept: 'application/json',
-//         Authorization: 'Token token="855df50978dc9afd6bf86579913c9f8b"'
-// 	}
-// }).then(res => console.log(res.data))
-
-
-
 // generate app
 const { createApp } = Vue
 createApp({
@@ -33,6 +19,7 @@ createApp({
             searchTerm: null,
             baseURL: 'https://favqs.com/api/quotes/',
             wasSearch: false,
+            hasQuotes: true,
         }
     },
     created () {
@@ -54,6 +41,7 @@ createApp({
                 }
             }).then(res => {
                 this.quotesObj = res.data.quotes
+                // set firstPage and lastPage booleans for pagination if a search occured
                 if (this.wasSearch){
                     if (res.data.page == 1) {
                         this.firstPage = false
@@ -65,33 +53,55 @@ createApp({
                     } else {
                         this.lastPage = true
                     }
+                    // prevent currentPage from exceeding actual current page if too many clicks happen before response
+                    this.currentPage = res.data.page
+                // hide pagination if random quote was selected
                 } else {
-                    this.firstPage = true
-                    this.lastPage = true
+                    this.firstPage = false
+                    this.lastPage = false
                 }
+                // set booleans if no results were found
+                if (res.data.quotes[0].body == 'No quotes found') {
+                    this.hasQuotes = false
+                    this.lastPage = false
+                } else {
+                    this.hasQuotes = true
+                }
+                // console.log(res.data)
             })
         },
+        // clear search field and stored term
         clearSearchField () {
             this.searchTermField = null
             this.searchTerm = null
         },
+        // set boolean to true if a search occurred
         wasSearchTrue() {
             this.wasSearch = true
         },
+        // set boolean to false if no search ocurred
         wasSearchFalse () {
             this.wasSearch = false
         },
+        // set search term from field when run (to avoid typing in field interfering with paginated results)
         updateSearchTerm () {
             this.searchTerm = this.searchTermField
         },
+        // reset page counter
         resetPage() {
             this.currentPage = 1
         },
+        // increment page counter
         nextPage () {
             this.currentPage++
         },
+        // decrement page counter
         prevPage () {
             this.currentPage--
-        }
+        },
+        // scroll to top of page
+        toTop () {
+            window.scrollTo(0,0)
+        },
     }
 }).mount('#app')
