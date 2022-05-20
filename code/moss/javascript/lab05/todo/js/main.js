b3ef087075f3fff198e627ebac5376f2dda5c12f
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-05-20 12:16:59 -0700

    ...to be continued

--- code/moss/javascript/lab05/todo/js/main.js
+++ code/moss/javascript/lab05/todo/js/main.js
@@ -1,9 +1,33 @@
 const App = {
     data () {
-        return {
-            message: 'Hello world!'
+        // let id = 0
+
+       return {
+            newTask : '',
+            incompleteTasks :[
+                'Django Library',
+                'Django Chirp',
+                'Javascript Vue ToDo',
+                'Javascript Vue Quotes',
+            ],
+            completeTasks :[
+
+            ]
         }
-    }
+    },
+    
+    methods: {
+        addTask() {
+            console.log(this.newTask)
+            // this.tasks.push({ id: id++, text: this.newTask })
+            this.tasks.push(this.newTask)
+        },
+
+        removeTask(tasks) {
+            this.incompleteTasks = tasks.value.filter((t) => t !== task)   
+        },  this.completeTasks
+    },
 }
 
-Vue.createApp(App).mount('#app')
+const app = Vue.createApp(App)
+app.mount('#app')
