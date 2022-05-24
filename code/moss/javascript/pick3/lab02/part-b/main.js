946c46f8fc4c206c9fd1fbcd4fbd85de2c54a5e8
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-05-24 15:22:11 -0700

    Update

--- code/moss/javascript/pick3/lab02/part-b/main.js
+++ code/moss/javascript/pick3/lab02/part-b/main.js
@@ -1,25 +1,30 @@
+let form = document.querySelector("form")
 let numberInput = document.getElementById("number-input")
 let numberArray = document.getElementById("number-array")
 let avrgButton = document.getElementById("average-btn")
 let avrgResult= document.getElementById("result")
 
+
 let number_array = []
 
-avrgButton.addEventListener("click", function() {
-
+avrgButton.addEventListener("click", function(e) {
+    
+    e.preventDefault()
+    
     let sum_number = 0
 
     let number = numberInput.value 
     console.log(`${number} is the input number`)
 
+
     number_array.push(number)
     console.log(`${number_array} is an array of input numbers`)
 
-    for (let numberarrays of numberArray){
-        sum_number += parseInt(numberarrays)
-        console.log(sum_number)
+    for (let i = 0; i < number_array.length; i++) {
+        sum_number += parseInt(number_array[i])
+        console.log(sum_number)   
     }
- 
+
     let total = number_array.length
     console.log(total)
 
@@ -29,4 +34,6 @@ avrgButton.addEventListener("click", function() {
     numberArray.innerText = `Numbers : ${number_array}`
     avrgResult.innerText = `Average : ${average}`
 
+    numberInput.value = '' 
 })
+
