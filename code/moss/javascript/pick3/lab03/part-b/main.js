946b10fe9014c8c3fa141838606d251417fa9388
Massimiliano Gallo <91701893+MossGallo@users.noreply.github.com>
2022-05-26 09:44:00 -0700

    Update

--- code/moss/javascript/pick3/lab03/part-b/main.js
+++ code/moss/javascript/pick3/lab03/part-b/main.js
@@ -1,19 +1,20 @@
-alert('\nWelcome to the BlackJack Advisor\n')
+let cardInput = document.getElementById("card-select")
+let addCardButton = document.getElementById("add-card")
+let adviseButton = document.getElementById("advise")
+let output = document.getElementById("output")
+let advisor = document.getElementById("advisor")
 
-let card_value = { 'A':1, '1':1, '2':2, '3':3,'4':4, '5':5,
-'6':6,'7':7, '8':8, '9':9,'10':10, 'J':10, 'Q':10, "K":10 }
+cardHand = []
 
-let user_1 = prompt('\nWhat is your first card?\n').toUpperCase()
-console.log(user_1)
+addCardButton.addEventListener("click", function() {
+    let card_choice = cardInput.value
+    cardHand.push(card_choice)
+    console.log(`${cardHand} is the players hand`)
+})
 
-let user_2 = prompt('\nWhat is your second card?\n').toUpperCase()
-console.log(user_2)
-
-let user_3 = prompt('\nWhat is your third card?\n').toUpperCase()
-console.log(user_3)
-
-let user_points = card_value[user_1] + card_value[user_2] + card_value[user_3]
-console.log(user_points)
+adviseButton.addEventListener("click", function() {
+        
+})
 
 if (user_points <= 17) {
     alert(`${user_points}, HIT`)
@@ -30,3 +31,18 @@ else if (user_points === 21 ) {
 else if (user_points > 21) {
     alert(`${user_points}, ALREADY BUSTED!`)
 }
+
+// let card_value = { 'A':1, '1':1, '2':2, '3':3,'4':4, '5':5,
+// '6':6,'7':7, '8':8, '9':9,'10':10, 'J':10, 'Q':10, "K":10 }
+
+// let user_1 = prompt('\nWhat is your first card?\n').toUpperCase()
+// console.log(user_1)
+
+// let user_2 = prompt('\nWhat is your second card?\n').toUpperCase()
+// console.log(user_2)
+
+// let user_3 = prompt('\nWhat is your third card?\n').toUpperCase()
+// console.log(user_3)
+
+// let user_points = card_value[user_1] + card_value[user_2] + card_value[user_3]
+// console.log(user_points)
